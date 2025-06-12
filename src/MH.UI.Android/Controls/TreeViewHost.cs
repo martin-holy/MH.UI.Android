820548218c0a79ed35d5fd31c671a975a091d5d2
martin-holy <martin.holy@hotmail.com>
2025-06-12 19:25:21 +0200

    TreeViewHost

--- src/MH.UI.Android/Controls/TreeViewHost.cs
+++ src/MH.UI.Android/Controls/TreeViewHost.cs
@@ -5,40 +5,26 @@ using Android.Views;
 using Android.Widget;
 using AndroidX.RecyclerView.Widget;
 using MH.UI.Controls;
-using MH.Utils;
 
 namespace MH.UI.Android.Controls;
 
 public class TreeViewHost : FrameLayout {
   private RecyclerView _recyclerView;
   private TreeViewHostAdapter _adapter;
-  private TreeView? _viewModel;
+  private TreeView _viewModel;
 
-  public TreeViewHost(Context context, TreeView? viewModel) : base(context) => _initialize(context, viewModel);
-  public TreeViewHost(Context context, IAttributeSet attrs, TreeView? viewModel) : base(context, attrs) => _initialize(context, viewModel);
+  public TreeView ViewModel { get => _viewModel; private set => _viewModel = value; }
+
+  public TreeViewHost(Context context, TreeView viewModel) : base(context) => _initialize(context, viewModel);
+  public TreeViewHost(Context context, IAttributeSet attrs, TreeView viewModel) : base(context, attrs) => _initialize(context, viewModel);
   protected TreeViewHost(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { }
 
-  private void _initialize(Context context, TreeView? viewModel) {
+  private void _initialize(Context context, TreeView viewModel) {
     LayoutInflater.From(context).Inflate(Resource.Layout.tree_view_host, this, true);
-    _adapter = new TreeViewHostAdapter(Context);
+    _adapter = new TreeViewHostAdapter(Context, viewModel.RootHolder);
     _recyclerView = FindViewById<RecyclerView>(Resource.Id.tree_recycler_view);
     _recyclerView.SetLayoutManager(new LinearLayoutManager(context));
     _recyclerView.SetAdapter(_adapter);
     ViewModel = viewModel;
-  }
-
-  public TreeView? ViewModel {
-    get => _viewModel;
-    set {
-      _viewModel = value;
-      _setItemsSource();
-    }
-  }
-
-  private void _setItemsSource() {
-    if (ViewModel == null) return;
-    var newFlatItems = Tree.ToFlatTreeItems(ViewModel.RootHolder);
-    //_updateTreeItemSubscriptions(ItemsSource as IEnumerable<FlatTreeItem>, newFlatItems);
-    _adapter.UpdateItems(newFlatItems);
   }
 }
