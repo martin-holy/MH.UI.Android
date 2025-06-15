371d14edd8fe96ef60dd219f5c28f1063e936726
martin-holy <martin.holy@hotmail.com>
2025-06-15 13:31:40 +0200

    TreeViewHost

--- src/MH.UI.Android/Controls/TreeViewHost.cs
+++ src/MH.UI.Android/Controls/TreeViewHost.cs
@@ -9,22 +9,26 @@ using MH.UI.Controls;
 namespace MH.UI.Android.Controls;
 
 public class TreeViewHost : RelativeLayout {
-  private RecyclerView _recyclerView;
-  private TreeViewHostAdapter _adapter;
-  private TreeView _viewModel;
+  private RecyclerView _recyclerView = null!;
+  private TreeViewHostAdapter _adapter = null!;
+  private TreeView _viewModel = null!;
 
-  public TreeView ViewModel { get => _viewModel; private set => _viewModel = value; }
+  public TreeView ViewModel {
+    get => _viewModel;
+    set {
+      _viewModel = value;
+      _adapter = new TreeViewHostAdapter(Context!, _viewModel);
+      _recyclerView.SetAdapter(_adapter);
+    }
+  }
 
-  public TreeViewHost(Context context, TreeView viewModel) : base(context) => _initialize(context, viewModel);
-  public TreeViewHost(Context context, IAttributeSet attrs, TreeView viewModel) : base(context, attrs) => _initialize(context, viewModel);
+  public TreeViewHost(Context context) : base(context) => _initialize(context);
+  public TreeViewHost(Context context, IAttributeSet attrs) : base(context, attrs) => _initialize(context);
   protected TreeViewHost(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { }
 
-  private void _initialize(Context context, TreeView viewModel) {
-    LayoutInflater.From(context).Inflate(Resource.Layout.tree_view_host, this, true);
-    _adapter = new TreeViewHostAdapter(Context, viewModel);
-    _recyclerView = FindViewById<RecyclerView>(Resource.Id.tree_recycler_view);
+  private void _initialize(Context context) {
+    LayoutInflater.From(context)!.Inflate(Resource.Layout.tree_view_host, this, true);
+    _recyclerView = FindViewById<RecyclerView>(Resource.Id.tree_recycler_view)!;
     _recyclerView.SetLayoutManager(new LinearLayoutManager(context));
-    _recyclerView.SetAdapter(_adapter);
-    ViewModel = viewModel;
   }
 }
