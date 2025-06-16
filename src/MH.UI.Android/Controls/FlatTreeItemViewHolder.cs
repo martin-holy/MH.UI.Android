04b0a06a3d9c2da2c0f4c3ad7c10abd03cef5e6c
martin-holy <martin.holy@hotmail.com>
2025-06-16 10:58:24 +0200

    FlatTreeItemViewHolder

--- src/MH.UI.Android/Controls/FlatTreeItemViewHolder.cs
+++ src/MH.UI.Android/Controls/FlatTreeItemViewHolder.cs
@@ -7,12 +7,25 @@ using MH.Utils.BaseClasses;
 
 namespace MH.UI.Android.Controls;
 
-public class FlatTreeItemViewHolder(View itemView, TreeView parent) : RecyclerView.ViewHolder(itemView) {
-  private readonly TreeView _parent = parent;
-  private readonly LinearLayout _container = (LinearLayout)itemView;  
-  private readonly ImageView _expandedIcon = itemView.FindViewById<ImageView>(Resource.Id.expanded_icon)!;
-  private readonly ImageView _icon = itemView.FindViewById<ImageView>(Resource.Id.icon)!;
-  private readonly TextView _name = itemView.FindViewById<TextView>(Resource.Id.name)!;
+public class FlatTreeItemViewHolder : RecyclerView.ViewHolder {
+  private readonly TreeView _parent;
+  private readonly LinearLayout _container;
+  private readonly ImageView _expandedIcon;
+  private readonly ImageView _icon;
+  private readonly TextView _name;
+
+  public FlatTreeItemViewHolder(View itemView, TreeView parent) : base(itemView) {
+    _parent = parent;
+
+    _container = (LinearLayout)itemView;
+    _container.Click += _onContainerClick;
+
+    _expandedIcon = itemView.FindViewById<ImageView>(Resource.Id.expanded_icon)!;
+    _expandedIcon.Click += _onExpandedChanged;
+
+    _icon = itemView.FindViewById<ImageView>(Resource.Id.icon)!;
+    _name = itemView.FindViewById<TextView>(Resource.Id.name)!;
+  }
 
   public FlatTreeItem? Item { get; private set; }
 
@@ -22,13 +35,9 @@ public class FlatTreeItemViewHolder(View itemView, TreeView parent) : RecyclerVi
 
     int indent = item.Level * _container.Resources?.GetDimensionPixelSize(Resource.Dimension.flat_tree_item_indent_size) ?? 32;
     _container.SetPadding(indent, _container.PaddingTop, _container.PaddingRight, _container.PaddingBottom);
-    _container.Click -= _onContainerClick;
-    _container.Click += _onContainerClick;
 
     _expandedIcon.Visibility = item.TreeItem.Items.Count > 0 ? ViewStates.Visible : ViewStates.Invisible;
     _expandedIcon.Selected = item.TreeItem.IsExpanded;
-    _expandedIcon.Click -= _onExpandedChanged;
-    _expandedIcon.Click += _onExpandedChanged;
 
     _icon.SetImageDrawable(Icons.GetIcon(_container.Context, item.TreeItem.Icon));
 
