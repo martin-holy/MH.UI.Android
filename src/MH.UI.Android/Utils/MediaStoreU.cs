93c6250eed26cb80993eb96974654b453b1b33aa
martin-holy <martin.holy@hotmail.com>
2025-06-18 10:01:01 +0200

    MediaStoreU: GetThumbnailBitmap and GetImageId methods

---
+++ src/MH.UI.Android/Utils/MediaStoreU.cs
@@ -0,0 +1,36 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Provider;
+
+namespace MH.UI.Android.Utils;
+
+public static class MediaStoreU {
+  public static Bitmap? GetThumbnailBitmap(string imagePath, Context context) {
+    var imageId = GetImageId(imagePath, context);
+    if (imageId == -1) return null;
+
+    return MediaStore.Images.Thumbnails.GetThumbnail(
+      context.ContentResolver, imageId, ThumbnailKind.MiniKind, new() { InSampleSize = 1 });
+  }
+
+  public static long GetImageId(string filePath, Context context) {
+    if (MediaStore.Images.Media.ExternalContentUri is not { } uri) return -1;
+
+    var cursor = context.ContentResolver?.Query(
+      uri,
+      [MediaStore.Images.Media.InterfaceConsts.Id],
+      $"{MediaStore.Images.Media.InterfaceConsts.Data}=?",
+      [filePath],
+      null);
+
+    if (cursor?.MoveToFirst() != true) {
+      cursor?.Close();
+      return -1;
+    }
+
+    var id = cursor.GetLong(0);
+    cursor.Close();
+
+    return id;
+  }
+}
