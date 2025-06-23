15461c180fe91494428c20c7097ca5f6bd6228f7
martin-holy <martin.holy@hotmail.com>
2025-06-23 13:11:03 +0200

    DisplayU

---
+++ src/MH.UI.Android/Utils/DisplayU.cs
@@ -0,0 +1,11 @@
+﻿using Android.Util;
+
+namespace MH.UI.Android.Utils;
+
+public class DisplayU {
+  public static DisplayMetrics Metrics { get; private set; }
+
+  public static void Init(DisplayMetrics displayMetrics) {
+    Metrics = displayMetrics;
+  }
+}
