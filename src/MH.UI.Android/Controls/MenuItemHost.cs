d021e32ad1e47c199dffb210a67948ae56eff575
martin-holy <martin.holy@hotmail.com>
2025-06-25 13:21:01 +0200

    MenuItemHost

---
+++ src/MH.UI.Android/Controls/MenuItemHost.cs
@@ -0,0 +1,37 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using MH.UI.Android.Utils;
+using MH.Utils.BaseClasses;
+
+namespace MH.UI.Android.Controls;
+
+public class MenuItemHost : LinearLayout {
+  private ImageView _icon = null!;
+  private TextView _text = null!;
+  private MenuItem _dataContext = null!;
+
+  public MenuItem DataContext { get => _dataContext; set { _dataContext = value; _bind(value); } }
+
+  public MenuItemHost(Context context) : base(context) => _initialize(context, null);
+  public MenuItemHost(Context context, IAttributeSet attrs) : base(context, attrs) => _initialize(context, attrs);
+  protected MenuItemHost(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) => _initialize(Context!, null);
+
+  private void _initialize(Context context, IAttributeSet? attrs) {
+    var height = DisplayU.GetDP(32);
+    LayoutInflater.From(context)!.Inflate(Resource.Layout.menu_item, this, true);
+    LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, height);
+    Orientation = Orientation.Horizontal;
+    SetBackgroundResource(Resource.Color.c_static_ba);
+    _icon = FindViewById<ImageView>(Resource.Id.icon)!;
+    _text = FindViewById<TextView>(Resource.Id.text)!;
+  }
+
+  private void _bind(MenuItem item) {
+    _icon.SetImageDrawable(Icons.GetIcon(Context, item.Icon));
+    _text.SetText(item.Text, TextView.BufferType.Normal);
+  }
+}
