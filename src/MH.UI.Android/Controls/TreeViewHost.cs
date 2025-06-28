14766ad4f090b5eeaed4621d291b2dec2ccc3ad1
martin-holy <martin.holy@hotmail.com>
2025-06-28 08:42:14 +0200

    ITreeViewHost

--- src/MH.UI.Android/Controls/TreeViewHost.cs
+++ src/MH.UI.Android/Controls/TreeViewHost.cs
@@ -5,18 +5,23 @@ using Android.Views;
 using Android.Widget;
 using AndroidX.RecyclerView.Widget;
 using MH.UI.Controls;
+using MH.Utils.Interfaces;
+using System;
 
 namespace MH.UI.Android.Controls;
 
-public class TreeViewHost : RelativeLayout {
+public class TreeViewHost : RelativeLayout, ITreeViewHost {
   private RecyclerView _recyclerView = null!;
   private TreeViewHostAdapter _adapter = null!;
   private TreeView _viewModel = null!;
+
+  public event EventHandler<bool>? HostIsVisibleChangedEvent;
 
   public TreeView ViewModel {
     get => _viewModel;
     set {
       _viewModel = value;
+      _viewModel.Host = this;
       _adapter = new TreeViewHostAdapter(Context!, _viewModel);
       _recyclerView.SetAdapter(_adapter);
     }
@@ -30,5 +35,20 @@ public class TreeViewHost : RelativeLayout {
     LayoutInflater.From(context)!.Inflate(Resource.Layout.tree_view_host, this, true);
     _recyclerView = FindViewById<RecyclerView>(Resource.Id.tree_recycler_view)!;
     _recyclerView.SetLayoutManager(new LinearLayoutManager(context));
+  }
+
+  protected override void OnVisibilityChanged(View changedView, [GeneratedEnum] ViewStates visibility) {
+    base.OnVisibilityChanged(changedView, visibility);
+    HostIsVisibleChangedEvent?.Invoke(this, visibility == ViewStates.Visible);
+  }
+
+  public void ExpandRootWhenReady(ITreeItem root) => root.IsExpanded = true;
+
+  public void ScrollToTop() {
+    // TODO PORT
+  }
+
+  public void ScrollToItems(object[] items, bool exactly) {
+    // TODO PORT
   }
 }
