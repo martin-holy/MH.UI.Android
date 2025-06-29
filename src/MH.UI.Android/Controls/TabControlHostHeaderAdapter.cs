958fd2805b1f3828b33b6dfab97819b13d751f63
martin-holy <martin.holy@hotmail.com>
2025-06-29 18:57:33 +0200

    TabControlHostHeaderAdapter

---
+++ src/MH.UI.Android/Controls/TabControlHostHeaderAdapter.cs
@@ -0,0 +1,20 @@
+﻿using Android.Views;
+using AndroidX.RecyclerView.Widget;
+
+namespace MH.UI.Android.Controls;
+
+public class TabControlHostHeaderAdapter(TabControlHost host) : RecyclerView.Adapter {
+  private readonly TabControlHost _host = host;
+
+  public override int ItemCount => _host.DataContext.Tabs.Count;
+
+  public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
+    var header = TabItemHeaderViewHolder.Create(parent);
+    header.SelectedAction = item => _host.DataContext.Selected = item;
+    return header;
+  }
+
+  public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    ((TabItemHeaderViewHolder)holder).Bind(_host.DataContext.Tabs[position]);
+  }
+}
