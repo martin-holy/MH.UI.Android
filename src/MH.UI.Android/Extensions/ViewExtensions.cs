82bdb12b8abc963223764d782dda74eb1cdb8c62
martin-holy <martin.holy@hotmail.com>
2025-07-26 09:58:48 +0200

    ViewExtensions: SetMargin, SetMarginUnified

---
+++ src/MH.UI.Android/Extensions/ViewExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Android.Views;
+
+namespace MH.UI.Android.Extensions;
+
+public static class ViewExtensions {
+  public static void SetMargin(this View view, int left, int top, int right, int bottom) {
+    var layoutParams = view.LayoutParameters as ViewGroup.MarginLayoutParams
+      ?? new ViewGroup.MarginLayoutParams(view.LayoutParameters
+        ?? new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent));
+    layoutParams.SetMargins(left, top, right, bottom);
+    view.LayoutParameters = layoutParams;
+  }
+
+  public static void SetMarginUnified(this View view, int value) =>
+    view.SetMargin(value, value, value, value);
+}
