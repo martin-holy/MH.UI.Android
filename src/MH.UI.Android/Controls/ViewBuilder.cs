5e59730ade085e2c3afd410198db3ac6e168394f
martin-holy <martin.holy@hotmail.com>
2025-08-02 18:52:35 +0200

    [N] ViewBuilder: CreateTreeItemExpandIconView

---
+++ src/MH.UI.Android/Controls/ViewBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Android.Content;
+using Android.Widget;
+
+namespace MH.UI.Android.Controls;
+
+public static class ViewBuilder {
+  public static ImageView CreateTreeItemExpandIconView(Context context) {
+    var size = context.Resources!.GetDimensionPixelSize(Resource.Dimension.icon_button_size);
+    var generalPadding = context.Resources!.GetDimensionPixelSize(Resource.Dimension.general_padding);
+    var icon = new ImageView(context) {
+      Clickable = true,
+      Focusable = true,
+      LayoutParameters = new LinearLayout.LayoutParams(size, size) {
+        MarginStart = generalPadding
+      }
+    };
+    icon.SetScaleType(ImageView.ScaleType.Center);
+    icon.SetImageResource(Resource.Drawable.tree_item_expanded_selector);
+
+    return icon;
+  }
+}
