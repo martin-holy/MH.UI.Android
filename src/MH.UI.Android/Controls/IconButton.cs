65dd1dc431417e35de370d5f63f599bbcf8829e7
martin-holy <martin.holy@hotmail.com>
2025-08-02 12:04:38 +0200

    IconButton

---
+++ src/MH.UI.Android/Controls/IconButton.cs
@@ -0,0 +1,15 @@
+﻿using Android.Content;
+using Android.Widget;
+using MH.UI.Android.Utils;
+
+namespace MH.UI.Android.Controls;
+
+public class IconButton : ImageButton {
+  public IconButton(Context context) : base(context) {
+    var size = context.Resources!.GetDimensionPixelSize(Resource.Dimension.icon_button_size);
+    LayoutParameters = new(size, size);
+  }
+
+  public void SetIcon(string iconName) =>
+    SetImageDrawable(Icons.GetIcon(Context, iconName));
+}
