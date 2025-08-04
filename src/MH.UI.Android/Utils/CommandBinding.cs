5d7e35be2bae94539bd8189c4f917f7aa2b6bf29
martin-holy <martin.holy@hotmail.com>
2025-08-04 10:57:59 +0200

    [N] CommandBinding

---
+++ src/MH.UI.Android/Utils/CommandBinding.cs
@@ -0,0 +1,40 @@
+﻿using Android.Views;
+using MH.Utils.Extensions;
+using System;
+using System.Windows.Input;
+
+namespace MH.UI.Android.Utils;
+
+public sealed class CommandBinding : IDisposable {
+  private readonly View _view;
+  private readonly ICommand _command;
+  private object? _parameter;
+  private bool _disposed;
+
+  public object? Parameter {
+    get => _parameter;
+    set {
+      _parameter = value;
+      _updateEnabledState();
+    }
+  }
+
+  public CommandBinding(View view, ICommand command) {
+    _view = view;
+    _view.Click += _onClick;
+    _command = command;
+    _updateEnabledState();
+  }
+
+  private void _onClick(object? sender, EventArgs e) =>
+    _command.ExecuteIfCan(_parameter);
+
+  private void _updateEnabledState() =>
+    _view.Enabled = _command.CanExecute(_parameter);
+
+  public void Dispose() {
+    if (_disposed) return;
+    _view.Click -= _onClick;
+    _disposed = true;
+  }
+}
