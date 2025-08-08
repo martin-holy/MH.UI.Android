11dd968031aea7820430b918d43c186c70be0e9d
martin-holy <martin.holy@hotmail.com>
2025-08-08 11:56:13 +0200

    [U] DialogHost

--- src/MH.UI.Android/Controls/DialogHost.cs
+++ src/MH.UI.Android/Controls/DialogHost.cs
@@ -1,8 +1,11 @@
 ﻿using Android.Content;
+using Android.Graphics.Drawables;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
 using AndroidX.Fragment.App;
+using MH.UI.Android.Extensions;
+using MH.UI.Android.Utils;
 using MH.Utils.BaseClasses;
 using System;
 using System.Threading.Tasks;
@@ -42,54 +45,102 @@ public class DialogHost : DialogFragment {
     _dataContext.TaskCompletionSource.TrySetResult(_dataContext.Result);
   }
 
-  public override global::Android.App.Dialog OnCreateDialog(Bundle savedInstanceState) {
+  public override global::Android.App.Dialog OnCreateDialog(Bundle? savedInstanceState) {
     var dialog = base.OnCreateDialog(savedInstanceState);
-    dialog.Window?.RequestFeature(WindowFeatures.NoTitle);
+
+    if (dialog.Window is { } window) {
+      window?.SetBackgroundDrawable(new ColorDrawable(Color.Transparent));
+      window?.RequestFeature(WindowFeatures.NoTitle);
+      window?.DecorView.SetPadding(0, 0, 0, 0);
+    }
+
     return dialog;
   }
 
-  public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    var context = Activity ?? throw new InvalidOperationException("Activity is null");
-    var rootLayout = new LinearLayout(context) {
-      Orientation = Orientation.Vertical,
-      LayoutParameters = new ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MatchParent,
-            ViewGroup.LayoutParams.WrapContent)
+  public override View OnCreateView(LayoutInflater? inflater, ViewGroup? container, Bundle? savedInstanceState) {
+    var context = container?.Context ?? Activity!;
+    var rootView = _createRootView(context);
+
+    var titleBar = _createTitleBarView(context);
+    titleBar.AddView(_createTitleIconView(context, _dataContext));
+    titleBar.AddView(_createTitleTextView(context, _dataContext));
+    titleBar.AddView(_createTitleCloseBtnView(context, _dataContext));
+    rootView.AddView(titleBar);
+
+    if (_contentViewFactory?.Invoke(context, _dataContext) is { } contentView)
+      rootView.AddView(contentView);
+
+    rootView.AddView(_createButtonsView(context, _dataContext));
+
+    return rootView;
+  }
+
+  private static LinearLayout _createRootView(Context context) {
+    var view = new LinearLayout(context) {
+      LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent),
+      Orientation = Orientation.Vertical
     };
+    view.SetBackgroundResource(Resource.Drawable.dialog_background);
 
-    // Title bar
-    var titleLayout = new LinearLayout(context) {
-      Orientation = Orientation.Horizontal,
-      Background = new global::Android.Graphics.Drawables.ColorDrawable(Color.DarkGray)
-    };
+    return view;
+  }
 
-    var titleText = new TextView(context) {
-      Text = _dataContext.Title,
-      Gravity = GravityFlags.CenterVertical,
-      TextSize = 18
-    };
-    titleLayout.AddView(titleText);
-    rootLayout.AddView(titleLayout);
-
-    // Content
-    var contentView = _contentViewFactory?.Invoke(context, _dataContext);
-    if (contentView != null) rootLayout.AddView(contentView);
-
-    // Buttons
-    var buttonsLayout = new LinearLayout(context) {
+  private static LinearLayout _createTitleBarView(Context context) {
+    var view = new LinearLayout(context) {
+      LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent),
       Orientation = Orientation.Horizontal
     };
-    buttonsLayout.SetGravity(GravityFlags.End);
+    view.SetGravity(GravityFlags.CenterVertical);
+    view.SetBackgroundResource(Resource.Color.c_black2);
+    view.SetPadding(context.Resources!.GetDimensionPixelSize(Resource.Dimension.general_padding));
 
-    foreach (var button in _dataContext.Buttons) {
+    return view;
+  }
+
+  private static ImageView _createTitleIconView(Context context, Dialog dataContext) {
+    var size = context.Resources!.GetDimensionPixelSize(Resource.Dimension.icon_size);
+    var view = new ImageView(context) {
+      LayoutParameters = new ViewGroup.LayoutParams(size, size)
+    };
+    view.SetImageDrawable(Icons.GetIcon(context, dataContext.Icon));
+
+    return view;
+  }
+
+  private static TextView _createTitleTextView(Context context, Dialog dataContext) {
+    var view = new TextView(context) {
+      LayoutParameters = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WrapContent) {
+        Weight = 1
+      },
+      Text = dataContext.Title,
+      TextSize = 18
+    };
+
+    return view;
+  }
+
+  private static IconButton _createTitleCloseBtnView(Context context, Dialog dataContext) {
+    var view = new IconButton(context); // todo command bind
+    view.SetImageDrawable(Icons.GetIcon(context, dataContext.Icon)); // todo X icon
+
+    return view;
+  }
+
+  private static LinearLayout _createButtonsView(Context context, Dialog dataContext) {
+    var view = new LinearLayout(context) {
+      Orientation = Orientation.Horizontal
+    };
+    view.SetGravity(GravityFlags.End);
+
+    // todo bind commands
+    foreach (var button in dataContext.Buttons) {
       var btn = new Button(context) {
         Text = ((RelayCommandBase)button.Command).Text
       };
-      btn.Click += (s, e) => button.Command?.Execute(_dataContext);
-      buttonsLayout.AddView(btn);
+      btn.Click += (s, e) => button.Command?.Execute(dataContext);
+      view.AddView(btn);
     }
 
-    rootLayout.AddView(buttonsLayout);
-    return rootLayout;
+    return view;
   }
 }
