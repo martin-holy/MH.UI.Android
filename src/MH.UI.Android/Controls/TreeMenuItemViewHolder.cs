51e89e9558383f98d4319baafd814a9a8e8f6beb
martin-holy <martin.holy@hotmail.com>
2025-08-19 12:26:51 +0200

    [N] TreeMenuItemViewHolder

---
+++ src/MH.UI.Android/Controls/TreeMenuItemViewHolder.cs
@@ -0,0 +1,120 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using MH.UI.Android.Extensions;
+using MH.UI.Android.Utils;
+using MH.Utils.BaseClasses;
+using System;
+
+namespace MH.UI.Android.Controls;
+
+public class TreeMenuItemViewHolder : RecyclerView.ViewHolder {
+  protected readonly LinearLayout _container;
+  private readonly ImageView _expandedIcon;
+  private readonly ImageView _icon;
+  private readonly TextView _name;
+  protected bool _disposed;
+
+  public FlatTreeItem? DataContext { get; private set; }
+
+  public TreeMenuItemViewHolder(Context context) : base(_createContainerView(context)) {
+    _icon = _createIconView(context);
+    _name = _createTextView(context);
+    _expandedIcon = _createTreeItemExpandIconView(context);
+    _container = (LinearLayout)ItemView;
+    _container.AddView(_icon);
+    _container.AddView(_name);
+    _container.AddView(_expandedIcon);
+    _container.Click += _onContainerClick;
+  }
+
+  protected override void Dispose(bool disposing) {
+    if (_disposed) return;
+    if (disposing) {
+      _container.Click -= _onContainerClick;
+    }
+    _disposed = true;
+    base.Dispose(disposing);
+  }
+
+  public virtual void Bind(FlatTreeItem? item) {
+    DataContext = item;
+    if (item == null || item.TreeItem is not MenuItem menuItem) return;
+
+    int indent = item.Level * ItemView.Resources!.GetDimensionPixelSize(Resource.Dimension.flat_tree_item_indent_size);
+    ItemView.SetPadding(indent, ItemView.PaddingTop, ItemView.PaddingRight, ItemView.PaddingBottom);
+
+    _icon.SetImageDrawable(Icons.GetIcon(ItemView.Context, menuItem.Icon));
+
+    _name.SetText(menuItem.Text, TextView.BufferType.Normal);
+
+    _expandedIcon.Visibility = menuItem.Items.Count > 0 ? ViewStates.Visible : ViewStates.Invisible;
+    _expandedIcon.Selected = menuItem.IsExpanded;
+  }
+
+  private void _onContainerClick(object? sender, EventArgs e) {
+    if (DataContext?.TreeItem is not MenuItem item) return;
+
+    if (item.Items.Count == 0) {
+      if (item.Command?.CanExecute(item.CommandParameter) == true)
+        item.Command.Execute(item.CommandParameter);
+
+      return;
+    }
+
+    item.IsExpanded = !item.IsExpanded;
+  }
+
+  private static LinearLayout _createContainerView(Context context) {
+    var container = new LinearLayout(context) {
+      LayoutParameters = new ViewGroup.LayoutParams(
+        ViewGroup.LayoutParams.MatchParent,
+        context.Resources!.GetDimensionPixelSize(Resource.Dimension.menu_item_height)),
+      Orientation = Orientation.Horizontal,
+      Clickable = true,
+      Focusable = true
+    };
+    container.SetGravity(GravityFlags.CenterVertical);
+    container.SetPadding(0);
+    container.SetBackgroundResource(Resource.Color.c_static_ba);
+
+    return container;
+  }
+
+  private static ImageView _createIconView(Context context) {
+    var size = context.Resources!.GetDimensionPixelSize(Resource.Dimension.icon_size);
+    var generalPadding = context.Resources!.GetDimensionPixelSize(Resource.Dimension.general_padding);
+    var view = new ImageView(context) {
+      LayoutParameters = new LinearLayout.LayoutParams(size, size) {
+        MarginStart = generalPadding,
+        MarginEnd = generalPadding
+      },
+    };
+
+    return view;
+  }
+
+  private static TextView _createTextView(Context context) {
+    var textView = new TextView(context) {
+      LayoutParameters = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WrapContent, 1f)
+    };
+    textView.SetTextColor(new Color(context.Resources!.GetColor(Resource.Color.c_static_fo, context.Theme)));
+    textView.SetSingleLine(true);
+
+    return textView;
+  }
+
+  private static ImageView _createTreeItemExpandIconView(Context context) {
+    var size = context.Resources!.GetDimensionPixelSize(Resource.Dimension.icon_button_size);
+    var icon = new ImageView(context) {
+      Focusable = true,
+      LayoutParameters = new LinearLayout.LayoutParams(size, size)
+    };
+    icon.SetScaleType(ImageView.ScaleType.Center);
+    icon.SetImageResource(Resource.Drawable.tree_item_expanded_selector);
+
+    return icon;
+  }
+}
