87083d0b290770ca9d0af0e05172f863cccb168d
martin-holy <martin.holy@hotmail.com>
2025-08-19 16:39:07 +0200

    [N] TreeMenuHostAdapter

---
+++ src/MH.UI.Android/Controls/TreeMenuHostAdapter.cs
@@ -0,0 +1,15 @@
+﻿using Android.Content;
+using Android.Views;
+using AndroidX.RecyclerView.Widget;
+
+namespace MH.UI.Android.Controls;
+
+public class TreeMenuHostAdapter(Context _context, TreeMenuHost _host)
+  : TreeViewHostAdapterBase(_context, _host.DataContext.RootHolder) {
+
+  public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) =>
+    new TreeMenuItemViewHolder(parent.Context!);
+
+  public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position) =>
+    ((TreeMenuItemViewHolder)holder).Bind(_items[position]);
+}
