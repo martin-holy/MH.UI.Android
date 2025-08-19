780172a43a1861e55130b52fbf4fb801d60c4e69
martin-holy <martin.holy@hotmail.com>
2025-08-19 16:40:00 +0200

    [N] TreeMenuHost

---
+++ src/MH.UI.Android/Controls/TreeMenuHost.cs
@@ -0,0 +1,12 @@
+﻿using Android.Content;
+using MH.UI.Controls;
+
+namespace MH.UI.Android.Controls;
+
+public class TreeMenuHost : TreeViewHostBase<TreeView, TreeViewHostAdapterBase> {
+  public TreeMenuHost(Context context, TreeView dataContext) : base(context, dataContext, null) {
+    DataContext.Host = this;
+    Adapter = new TreeMenuHostAdapter(context, this);
+    _recyclerView.SetAdapter(Adapter);
+  }
+}
