c8607e3bc0a68696dc488edd460e008fd45aaaa2
martin-holy <martin.holy@hotmail.com>
2025-08-20 09:18:56 +0200

    [U] TreeMenuFactory

--- src/MH.UI.Android/Utils/TreeMenuFactory.cs
+++ src/MH.UI.Android/Utils/TreeMenuFactory.cs
@@ -3,6 +3,7 @@ using Android.Content.Res;
 using Android.Graphics;
 using Android.Views;
 using Android.Widget;
+using AndroidX.RecyclerView.Widget;
 using MH.UI.Android.Controls;
 using MH.UI.Android.Extensions;
 using MH.UI.Controls;
@@ -13,26 +14,20 @@ using System.Collections.Generic;
 namespace MH.UI.Android.Utils;
 
 public static class TreeMenuFactory {
-  public static PopupWindow CreateMenu(Context context, TreeView dataContext) {
+  public static PopupWindow CreateMenu(Context context, TreeView dataContext, View anchor) {
     var treeMenu = new TreeMenuHost(context, dataContext);
     treeMenu.SetBackgroundResource(Resource.Drawable.view_border);
     treeMenu.SetPadding(DisplayU.DpToPx(1));
 
     var popup = new PopupWindow(treeMenu, ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent, true);
 
-    treeMenu.Post(() => {
-      var totalHeight = treeMenu.Adapter!.ItemCount * context.Resources!.GetDimensionPixelSize(Resource.Dimension.menu_item_height);
-      var maxHeight = DisplayU.Metrics.HeightPixels;
-      var totalWidth = _getTreeMenuWidth(context.Resources!, context, treeMenu.Adapter.Items);
-      var screenWidth = DisplayU.Metrics.WidthPixels;
-
-      popup.Update(Math.Min(totalWidth, screenWidth), Math.Min(totalHeight, maxHeight));
-    });
+    var observer = new TreeMenuAdapterDataObserver(context, treeMenu, popup, anchor);
+    treeMenu.Adapter!.RegisterAdapterDataObserver(observer);
 
     return popup;
   }
 
-  private static int _getTreeMenuWidth(Resources res, Context context, IEnumerable<FlatTreeItem> items) {    
+  private static int _getTreeMenuWidth(Resources res, Context context, IEnumerable<FlatTreeItem> items) {
     var generalPadding = res.GetDimensionPixelSize(Resource.Dimension.general_padding);
 
     var textView = new TextView(context);
@@ -53,5 +48,33 @@ public static class TreeMenuFactory {
     var expander = res.GetDimensionPixelSize(Resource.Dimension.icon_button_size);
 
     return (int)(padding + indent + icon + textPadding + maxTextWidth + expander);
+  }
+
+  private static int _getTreeMenuHeight(View anchor) {
+    var location = new int[2];
+    anchor.GetLocationOnScreen(location);
+
+    var anchorBottom = location[1] + anchor.Height;
+    var screenHeight = DisplayU.Metrics.HeightPixels;
+
+    return screenHeight - anchorBottom;
+  }
+
+  private class TreeMenuAdapterDataObserver(Context context, TreeMenuHost treeMenu, PopupWindow popup, View anchor)
+    : RecyclerView.AdapterDataObserver {
+
+    public override void OnChanged() {
+      base.OnChanged();
+      UpdatePopupSize();
+    }
+
+    public void UpdatePopupSize() {
+      var totalWidth = _getTreeMenuWidth(context.Resources!, context, treeMenu.Adapter!.Items);
+      var totalHeight = treeMenu.Adapter!.ItemCount * context.Resources!.GetDimensionPixelSize(Resource.Dimension.menu_item_height);
+      var maxWidth = DisplayU.Metrics.WidthPixels;
+      var maxHeight = _getTreeMenuHeight(anchor);
+
+      popup.Update(Math.Min(totalWidth, maxWidth), Math.Min(totalHeight, maxHeight));
+    }
   }
 }
