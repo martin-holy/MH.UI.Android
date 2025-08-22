2f776961a722180970a777fbba6b09ea89b86565
martin-holy <martin.holy@hotmail.com>
2025-08-22 17:13:37 +0200

    [U] ViewExtensions: SetMargin

--- src/MH.UI.Android/Extensions/ViewExtensions.cs
+++ src/MH.UI.Android/Extensions/ViewExtensions.cs
@@ -3,18 +3,19 @@
 namespace MH.UI.Android.Extensions;
 
 public static class ViewExtensions {
-  public static void SetMargin(this View view, int left, int top, int right, int bottom) {
+  public static T SetMargin<T>(this T view, int left, int top, int right, int bottom) where T : View {
     var layoutParams = view.LayoutParameters as ViewGroup.MarginLayoutParams
       ?? new ViewGroup.MarginLayoutParams(view.LayoutParameters
         ?? new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent));
     layoutParams.SetMargins(left, top, right, bottom);
     view.LayoutParameters = layoutParams;
+    return view;
   }
 
   public static void SetMargin(this ViewGroup.MarginLayoutParams layoutParams, int value) =>
     layoutParams.SetMargins(value, value, value, value);
 
-  public static void SetMargin(this View view, int value) =>
+  public static T SetMargin<T>(this T view, int value) where T : View =>
     view.SetMargin(value, value, value, value);
 
   public static void SetPadding(this View view, int value) =>
