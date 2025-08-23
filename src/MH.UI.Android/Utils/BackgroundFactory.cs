ed1667bfefe5503cdaf556707a557bd85e3bce4f
martin-holy <martin.holy@hotmail.com>
2025-08-23 11:01:26 +0200

    [N] BackgroundFactory

---
+++ src/MH.UI.Android/Utils/BackgroundFactory.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using AndroidX.Core.Content;
+using System.Collections.Concurrent;
+
+namespace MH.UI.Android.Utils;
+
+public static class BackgroundFactory {
+  private static readonly ConcurrentDictionary<string, GradientDrawable> _cache = new();
+
+  public static GradientDrawable Create(int fillResId, int strokeColorResId, int strokeWidthResId, int cornerRadiusResId) {
+    var key = $"{fillResId}|{strokeColorResId}|{strokeWidthResId}|{cornerRadiusResId}";
+
+    return _cache.GetOrAdd(key, _ => {
+      var fill = new Color(ContextCompat.GetColor(Application.Context, fillResId));
+      var stroke = new Color(ContextCompat.GetColor(Application.Context, strokeColorResId));
+      var strokeWidth = (int)Application.Context.Resources.GetDimension(strokeWidthResId);
+      var cornerRadius = Application.Context.Resources.GetDimension(cornerRadiusResId);
+      var shape = new GradientDrawable();
+      shape.SetColor(fill);
+      shape.SetStroke(strokeWidth, stroke);
+      shape.SetCornerRadius(cornerRadius);
+      return shape;
+    });
+  }
+
+  public static GradientDrawable RoundDark() =>
+    Create(
+      Resource.Color.c_black5,
+      Resource.Color.c_black,
+      Resource.Dimension.border_stroke_width,
+      Resource.Dimension.border_corner_radius);
+
+  public static GradientDrawable RoundDarker() =>
+    Create(
+      Resource.Color.c_black2,
+      Resource.Color.c_black,
+      Resource.Dimension.border_stroke_width,
+      Resource.Dimension.border_corner_radius);
+}
