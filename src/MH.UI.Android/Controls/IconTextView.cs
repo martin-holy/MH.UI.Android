e70c5b4eb80d728f49c1d53270362036935e4fc7
martin-holy <martin.holy@hotmail.com>
2025-08-23 10:54:44 +0200

    [N] IconTextView

---
+++ src/MH.UI.Android/Controls/IconTextView.cs
@@ -0,0 +1,60 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Core.Content;
+using MH.UI.Android.Extensions;
+using MH.UI.Android.Utils;
+
+namespace MH.UI.Android.Controls;
+
+public class IconTextView : LinearLayout {
+  private readonly IconView _icon;
+  private readonly TextView _text;
+  private readonly ColorStateList? _defaultTextColors;
+  private readonly int _textPadding;
+
+  public IconTextView(Context context) : base(context) {
+    Orientation = global::Android.Widget.Orientation.Horizontal;
+    Background = BackgroundFactory.RoundDark();
+    SetGravity(GravityFlags.CenterVertical);
+
+    var generalPadding = context.Resources!.GetDimensionPixelSize(Resource.Dimension.general_padding);
+
+    _icon = new IconView(context).SetMargin(generalPadding, 0, generalPadding, 0);
+    _text = new TextView(context);
+
+    AddView(_icon);
+    AddView(_text);
+
+    _defaultTextColors = _text.TextColors;
+    _textPadding = _text.PaddingLeft;
+  }
+
+  public IconTextView BindIcon(string? iconName, int? colorId = null) {
+    if (!_icon.SetVisibleIf(!string.IsNullOrEmpty(iconName))) return this;
+    _icon.Bind(iconName!, colorId);
+    return this;
+  }
+
+  public IconTextView BindIcon(int? drawableId, int? colorId = null) {
+    if (!_icon.SetVisibleIf(drawableId != null)) return this;
+    _icon.Bind((int)drawableId!, colorId);
+    return this;
+  }
+
+  public IconTextView BindText(string text, int? colorId = null) {
+    if (!_text.SetVisibleIf(!string.IsNullOrEmpty(text))) return this;
+
+    _text.Text = text;
+    _text.SetPadding(_icon.Visibility == ViewStates.Visible ? 0 : _textPadding, _textPadding, _textPadding, _textPadding);
+
+    if (colorId != null)
+      _text.SetTextColor(new Color(ContextCompat.GetColor(Context, (int)colorId)));
+    else
+      _text.SetTextColor(_defaultTextColors);
+
+    return this;
+  }
+}
