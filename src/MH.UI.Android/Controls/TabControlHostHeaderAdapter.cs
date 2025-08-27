34e837d9081cebe276f6161734daafaab261bf35
martin-holy <martin.holy@hotmail.com>
2025-08-27 10:46:38 +0200

    [C] TabItemHeaderViewHolder

--- src/MH.UI.Android/Controls/TabControlHostHeaderAdapter.cs
+++ src/MH.UI.Android/Controls/TabControlHostHeaderAdapter.cs
@@ -7,11 +7,8 @@ namespace MH.UI.Android.Controls;
 public class TabControlHostHeaderAdapter(TabControl _tabControl) : RecyclerView.Adapter {
   public override int ItemCount => _tabControl.Tabs.Count;
 
-  public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
-    var header = TabItemHeaderViewHolder.Create(parent);
-    header.SelectedAction = item => _tabControl.Selected = item;
-    return header;
-  }
+  public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) =>
+    new TabItemHeaderViewHolder(parent.Context!, _tabControl.SelectTabCommand);
 
   public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position) {
     ((TabItemHeaderViewHolder)holder).Bind(_tabControl.Tabs[position], _tabControl.TabStrip.IconTextVisibility);
--- src/MH.UI.Android/Controls/TabItemHeaderViewHolder.cs
+++ src/MH.UI.Android/Controls/TabItemHeaderViewHolder.cs
@@ -1,31 +1,37 @@
-﻿using Android.Views;
+﻿using Android.Content;
+using Android.Views;
 using Android.Widget;
 using AndroidX.RecyclerView.Widget;
+using MH.UI.Android.Extensions;
 using MH.UI.Android.Utils;
 using MH.UI.Controls;
+using MH.Utils.Extensions;
 using MH.Utils.Interfaces;
-using System;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace MH.UI.Android.Controls;
 
 public class TabItemHeaderViewHolder : RecyclerView.ViewHolder {
-  private readonly ImageView _icon;
+  protected readonly LinearLayout _container;
+  private readonly IconView _icon;
   private readonly TextView _name;
+  private bool _disposed;
+  private readonly CommandBinding _selectItemCommandBinding;
 
-  public IListItem? Item { get; private set; }
-  public Action<IListItem>? SelectedAction { get; set; }
+  public IListItem? DataContext { get; private set; }
 
-  public TabItemHeaderViewHolder(View itemView) : base(itemView) {
-    itemView.Click += _onContainerClick;
-    _icon = itemView.FindViewById<ImageView>(Resource.Id.icon)!;
-    _name = itemView.FindViewById<TextView>(Resource.Id.text)!;
+  public TabItemHeaderViewHolder(Context context, ICommand selectItemCommand) : base(_createContainerView(context)) {
+    _icon = new(context);
+    _name = new(context);
+    _container = (LinearLayout)ItemView;
+    _container.AddView(_icon);
+    _container.AddView(_name);
+    _selectItemCommandBinding = new CommandBinding(_container, selectItemCommand);
   }
 
-  public static TabItemHeaderViewHolder Create(ViewGroup parent) =>
-    new(LayoutInflater.From(parent.Context)!.Inflate(Resource.Layout.tab_item_header, parent, false)!);
-
   public void Bind(IListItem? item, IconTextVisibility itv) {
-    Item = item;
+    _setDataContext(DataContext, item);
     if (item == null) return;
 
     var isIconVisible = itv.HasFlag(IconTextVisibility.Icon);
@@ -39,12 +45,43 @@ public class TabItemHeaderViewHolder : RecyclerView.ViewHolder {
     _name.SetPadding(isIconVisible
       ? ItemView.Resources!.GetDimensionPixelSize(Resource.Dimension.general_padding)
       : 0, 0, 0, 0);
-    
+
     ItemView.Selected = item.IsSelected;
+    _selectItemCommandBinding.Parameter = item;
   }
 
-  private void _onContainerClick(object? sender, EventArgs e) {
-    if (SelectedAction != null && Item != null)
-      SelectedAction(Item);
+  private void _setDataContext(IListItem? oldValue, IListItem? newValue) {
+    if (oldValue != null) oldValue.PropertyChanged -= _onDataContextPropertyChanged;
+    if (newValue != null) newValue.PropertyChanged += _onDataContextPropertyChanged;
+  }
+
+  private void _onDataContextPropertyChanged(object? sender, PropertyChangedEventArgs e) {
+    if (sender is not IListItem listItem) return;
+    if (e.Is(nameof(IListItem.Name)))
+      _name.Text = listItem.Name;
+  }
+
+  protected override void Dispose(bool disposing) {
+    if (_disposed) return;
+    if (disposing) {
+      _selectItemCommandBinding.Dispose();
+      _setDataContext(DataContext, null);
+    }
+    _disposed = true;
+    base.Dispose(disposing);
+  }
+
+  private static LinearLayout _createContainerView(Context context) {
+    var container = new LinearLayout(context) {
+      LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent),
+      Orientation = Orientation.Horizontal,
+      Clickable = true,
+      Focusable = true
+    };
+    container.SetGravity(GravityFlags.CenterVertical);
+    container.SetPadding(context.Resources!.GetDimensionPixelSize(Resource.Dimension.general_padding));
+    container.SetBackgroundResource(Resource.Drawable.tab_item_header_background);
+
+    return container;
   }
 }
