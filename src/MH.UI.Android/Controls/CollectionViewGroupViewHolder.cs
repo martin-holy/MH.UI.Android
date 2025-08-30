6fb1f2fedb741df86d1ce9580f5b216adcc2fa04
martin-holy <martin.holy@hotmail.com>
2025-08-30 17:46:01 +0200

    [U] CollectionViewGroupViewHolder: SourceCount

--- src/MH.UI.Android/Controls/CollectionViewGroupViewHolder.cs
+++ src/MH.UI.Android/Controls/CollectionViewGroupViewHolder.cs
@@ -1,13 +1,28 @@
 ﻿using Android.Content;
+using Android.Widget;
 using MH.UI.Android.Extensions;
 using MH.UI.Android.Utils;
+using MH.UI.Interfaces;
+using MH.Utils.BaseClasses;
 
 namespace MH.UI.Android.Controls;
 
 public class CollectionViewGroupViewHolder : FlatTreeItemViewHolderBase {
+  private readonly TextView _sourceCount;
+
   public CollectionViewGroupViewHolder(Context context, IAndroidTreeViewHost treeViewHost) : base(context, treeViewHost) {
+    _sourceCount = new TextView(context);
+    _container.AddView(_sourceCount);
+
     var gp = context.Resources!.GetDimensionPixelSize(Resource.Dimension.general_padding);
     _container.SetMargin(gp, gp, gp, 0);
     _container.Background = BackgroundFactory.Dark();
+  }
+
+  public override void Bind(FlatTreeItem? item) {
+    base.Bind(item);
+    if (item is not { TreeItem: ICollectionViewGroup group}) return;
+
+    _sourceCount.Text = group.SourceCount.ToString();
   }
 }
