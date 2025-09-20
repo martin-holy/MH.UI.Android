ed0ab832905e25d0611e988a91432d495d0cbd14
martin-holy <martin.holy@hotmail.com>
2025-09-20 15:22:30 +0200

    LayoutParamsExtensions: WithMargin

---
+++ src/MH.UI.Android/Extensions/LayoutParamsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Android.Views;
+
+namespace MH.UI.Android.Extensions;
+
+public static class LayoutParamsExtensions {
+  public static T WithMargin<T>(this T layoutParams, int left, int top, int right, int bottom)
+    where T : ViewGroup.MarginLayoutParams {
+    layoutParams.SetMargins(left, top, right, bottom);
+    return layoutParams;
+  }
+
+  public static T WithMargin<T>(this T layoutParams, int all)
+    where T : ViewGroup.MarginLayoutParams {
+    layoutParams.SetMargins(all, all, all, all);
+    return layoutParams;
+  }
+}
