a684f40d3c849df71bd73552908061c52bb6b13d
martin-holy <martin.holy@hotmail.com>
2025-09-22 10:55:39 +0200

    LPU: Match, Wrap

---
+++ src/MH.UI.Android/Utils/LPU.cs
@@ -0,0 +1,8 @@
+﻿using Android.Views;
+
+namespace MH.UI.Android.Utils;
+
+public static class LPU {
+  public const int Match = ViewGroup.LayoutParams.MatchParent;
+  public const int Wrap = ViewGroup.LayoutParams.WrapContent;
+}
