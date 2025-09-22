84df5907c21fb3bb1ff909af328a767ff6cfbc9a
martin-holy <martin.holy@hotmail.com>
2025-09-22 13:01:35 +0200

    dimens: spacing

--- src/MH.UI.Android/Utils/DimensU.cs
+++ src/MH.UI.Android/Utils/DimensU.cs
@@ -5,12 +5,12 @@ namespace MH.UI.Android.Utils;
 public static class DimensU {
   public static int IconSize { get; private set; }
   public static int IconButtonSize { get; private set; }
-  public static int GeneralPadding { get; private set; }
+  public static int Spacing { get; private set; }
 
   public static void Init(Context context) {
     var res = context.Resources!;
     IconSize = res.GetDimensionPixelSize(Resource.Dimension.icon_size);
     IconButtonSize = res.GetDimensionPixelSize(Resource.Dimension.icon_button_size);
-    GeneralPadding = res.GetDimensionPixelSize(Resource.Dimension.general_padding);
+    Spacing = res.GetDimensionPixelSize(Resource.Dimension.spacing);
   }
 }
