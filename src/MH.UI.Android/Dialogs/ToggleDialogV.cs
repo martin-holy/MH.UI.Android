d2ff164fde85671294de152d6a4c591b72b247a4
martin-holy <martin.holy@hotmail.com>
2025-09-22 12:38:33 +0200

    ToggleDialogV: LayoutParams

--- src/MH.UI.Android/Dialogs/ToggleDialogV.cs
+++ src/MH.UI.Android/Dialogs/ToggleDialogV.cs
@@ -16,43 +16,29 @@ public class ToggleDialogV : LinearLayout, IDialogContentV {
   private readonly TextView _message;
 
   public ToggleDialogV(Context context) : base(context) {
-    _createThisView();
+    Orientation = Orientation.Horizontal;
+    SetMinimumWidth(DisplayU.DpToPx(300));
+    SetPadding(0, DisplayU.DpToPx(10), 0, DisplayU.DpToPx(10));
+    SetGravity(GravityFlags.CenterVertical);
 
-    _icon = new IconView(context, DisplayU.DpToPx(32))
-      .SetMargin(DisplayU.DpToPx(10));
+    _icon = new IconView(context);
+    _item = new IconTextView(context);
+    _message = new TextView(context);
+    var itemAndMessage = new LinearLayout(context) { Orientation = Orientation.Vertical };
 
-    _item = new IconTextView(context)
-      .SetMargin(DisplayU.DpToPx(5));
+    itemAndMessage.AddView(_item, new LayoutParams(LPU.Wrap, LPU.Wrap).WithMargin(DisplayU.DpToPx(5)));
+    itemAndMessage.AddView(_message, new LayoutParams(LPU.Wrap, LPU.Wrap)
+      .WithMargin(DisplayU.DpToPx(5), DisplayU.DpToPx(5), DisplayU.DpToPx(10), DisplayU.DpToPx(5)));
 
-    _message = new TextView(context)
-      .SetMargin(DisplayU.DpToPx(5), DisplayU.DpToPx(5), DisplayU.DpToPx(10), DisplayU.DpToPx(5));
-
-    var itemAndMessage = new LinearLayout(context) {
-      Orientation = Orientation.Vertical,
-      LayoutParameters = new LayoutParams(LayoutParams.WrapContent, LayoutParams.WrapContent, 1f)
-    };
-
-    itemAndMessage.AddView(_item);
-    itemAndMessage.AddView(_message);
-
-    AddView(_icon);
-    AddView(itemAndMessage);
+    AddView(_icon, new LayoutParams(DisplayU.DpToPx(32), DisplayU.DpToPx(32)).WithMargin(DisplayU.DpToPx(10)));
+    AddView(itemAndMessage, new LayoutParams(LPU.Wrap, LPU.Wrap, 1f));
   }
 
   public View Bind(Dialog dataContext) {
     if (dataContext is not ToggleDialog vm) throw new InvalidOperationException();
     _icon.Bind(Res.IconQuestion);
     _item.BindIcon(vm.Item!.Icon).BindText(vm.Item.Name);
-    _message.SetText(vm.Message, TextView.BufferType.Normal);
+    _message.Text = vm.Message;
     return this;
-  }
-
-  private void _createThisView() {
-    Orientation = Orientation.Horizontal;
-    LayoutParameters = new LayoutParams(LayoutParams.MatchParent, LayoutParams.WrapContent);
-
-    SetMinimumWidth(DisplayU.DpToPx(300));
-    SetPadding(0, DisplayU.DpToPx(10), 0, DisplayU.DpToPx(10));
-    SetGravity(GravityFlags.CenterVertical);
   }
 }
