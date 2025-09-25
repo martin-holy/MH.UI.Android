7b7201d51f71cc3350d6ce8346d74db1d504d154
martin-holy <martin.holy@hotmail.com>
2025-09-25 12:13:03 +0200

    [C] ViewExtensions: SetMargin removed

--- src/MH.UI.Android/Extensions/ViewExtensions.cs
+++ src/MH.UI.Android/Extensions/ViewExtensions.cs
@@ -4,18 +4,6 @@ using Android.Widget;
 namespace MH.UI.Android.Extensions;
 
 public static class ViewExtensions {
-  public static T SetMargin<T>(this T view, int left, int top, int right, int bottom) where T : View {
-    var layoutParams = view.LayoutParameters as ViewGroup.MarginLayoutParams
-      ?? new ViewGroup.MarginLayoutParams(view.LayoutParameters
-        ?? new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent));
-    layoutParams.SetMargins(left, top, right, bottom);
-    view.LayoutParameters = layoutParams;
-    return view;
-  }
-
-  public static T SetMargin<T>(this T view, int value) where T : View =>
-    view.SetMargin(value, value, value, value);
-
   public static void SetPadding(this View view, int value) =>
     view.SetPadding(value, value, value, value);
 
