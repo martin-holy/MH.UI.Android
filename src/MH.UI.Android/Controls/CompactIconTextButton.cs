f1dc33d3c5ecd5f1329c7c03cde4ac8ad505a160
martin-holy <martin.holy@hotmail.com>
2025-09-25 12:20:55 +0200

    [N] CompactIconTextButton

---
+++ src/MH.UI.Android/Controls/CompactIconTextButton.cs
@@ -0,0 +1,24 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Core.Content;
+using MH.UI.Android.Utils;
+
+namespace MH.UI.Android.Controls;
+
+public class CompactIconTextButton : FrameLayout {
+  public IconView Icon { get; }
+  public TextView Text { get; }
+
+  public CompactIconTextButton(Context context) : base(context) {
+    Background = ContextCompat.GetDrawable(context, Resource.Drawable.image_button_background);
+    
+    Icon = new IconView(context);
+    Icon.SetPadding(DimensU.Spacing, DimensU.Spacing, DimensU.Spacing * 2, DimensU.Spacing * 2);
+    Text = new TextView(context);
+    Text.SetPadding(0, 0, DisplayU.DpToPx(2), 0);
+
+    AddView(Icon, new LayoutParams(DimensU.IconButtonSize, DimensU.IconButtonSize) { Gravity = GravityFlags.Center });
+    AddView(Text, new LayoutParams(LPU.Wrap, LPU.Wrap) { Gravity = GravityFlags.Right | GravityFlags.Bottom });
+  }
+}
