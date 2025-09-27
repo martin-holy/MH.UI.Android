fac7c432aaf91cc78f62df1f0b0103030a7f65f1
martin-holy <martin.holy@hotmail.com>
2025-09-27 14:00:39 +0200

    [N] TreeMenu

---
+++ src/MH.UI.Android/Controls/TreeMenu.cs
@@ -0,0 +1,34 @@
+﻿using Android.Content;
+using Android.Views;
+using MH.UI.Controls;
+using MH.Utils.BaseClasses;
+using System;
+using System.Collections.Generic;
+
+namespace MH.UI.Android.Controls;
+
+public class TreeMenu(Context _context, Func<object, IEnumerable<MenuItem>> _itemMenuFactory) {
+  private TreeView? _itemMenuVM;
+  private TreeMenuHost? _itemMenuV;
+
+  public void ShowItemMenu(View anchor, object? item) {
+    if (item == null) return;
+    if (_itemMenuVM == null) {
+      _itemMenuVM = new();
+      _itemMenuV = new TreeMenuHost(_context, _itemMenuVM);
+    }
+
+    if (_itemMenuFactory(item) is not { } menuItems) return;
+
+    _itemMenuVM.RootHolder.Execute(items => {
+      items.Clear();
+      foreach (var menuItem in menuItems)
+        items.Add(menuItem);
+    });
+
+    if (_itemMenuV == null || _itemMenuVM.RootHolder.Count == 0) return;
+    _itemMenuV.Observer.MenuAnchor = anchor;
+    _itemMenuV.Observer.UpdatePopupSize();
+    _itemMenuV.Popup.ShowAsDropDown(anchor);
+  }
+}
