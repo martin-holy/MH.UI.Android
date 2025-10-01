994fafa310771b0b6bae901c7568e84b2d0b1af7
martin-holy <martin.holy@hotmail.com>
2025-10-01 15:27:51 +0200

    [C] TreeViewHost

--- src/MH.UI.Android/Controls/TreeViewHost.cs
+++ src/MH.UI.Android/Controls/TreeViewHost.cs
@@ -10,25 +10,14 @@ using System.Collections.Generic;
 namespace MH.UI.Android.Controls;
 
 public class TreeViewHost : TreeViewHostBase<TreeView, TreeViewHostAdapter>, ITreeViewHost {
-  private readonly SelectionBar _selectionBar;
-
   public TreeViewHost(Context context, TreeView dataContext, Func<object, IEnumerable<MenuItem>>? itemMenuFactory)
     : base(context, dataContext, itemMenuFactory) {
     DataContext.Host = this;
     Adapter = new TreeViewHostAdapter(context, this);
     _recyclerView.SetAdapter(Adapter);
-    _selectionBar = new SelectionBar(context, DataContext.SelectedTreeItems);
 
-    AddView(_selectionBar, new LayoutParams(LPU.Wrap, LPU.Wrap)
+    AddView(new SelectionBar(context, DataContext.SelectedTreeItems), new LayoutParams(LPU.Wrap, LPU.Wrap)
       .WithMargin(DimensU.Spacing)
       .WithRule(LayoutRules.AlignParentRight));
-  }
-
-  protected override void Dispose(bool disposing) {
-    if (_disposed) return;
-    if (disposing) {
-      _selectionBar.Dispose();
-    }
-    base.Dispose(disposing);
   }
 }
