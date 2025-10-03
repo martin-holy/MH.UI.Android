020b50222b3e7e77705d5a9d1f6f22cf6fe220ea
martin-holy <martin.holy@hotmail.com>
2025-10-03 09:48:38 +0200

    [C] GroupByDialogV

--- src/MH.UI.Android/Dialogs/GroupByDialogV.cs
+++ src/MH.UI.Android/Dialogs/GroupByDialogV.cs
@@ -2,22 +2,20 @@
 using Android.Views;
 using Android.Widget;
 using MH.UI.Android.Controls;
-using MH.UI.Android.Utils;
-using MH.UI.Controls;
-using MH.UI.Dialogs;
-using System;
 using MH.UI.Android.Extensions;
+using MH.UI.Android.Utils;
+using MH.UI.Dialogs;
 
 namespace MH.UI.Android.Dialogs;
 
-public sealed class GroupByDialogV : LinearLayout, IDialogContentV {
+public sealed class GroupByDialogV : LinearLayout {
   private readonly TreeViewHost _groupByItems;
   private readonly RadioButton _isGroupBy;
   private readonly RadioButton _isThenBy;
   private readonly CheckBox _isRecursive;
   private bool _disposed;
 
-  public GroupByDialog? DataContext { get; private set; }
+  public GroupByDialog DataContext { get; private set; }
 
   public GroupByDialogV(Context context, GroupByDialog dataContext) : base(context) {
     DataContext = dataContext;
@@ -29,33 +27,23 @@ public sealed class GroupByDialogV : LinearLayout, IDialogContentV {
 
     _groupByItems = new TreeViewHost(context, dataContext.TreeView, null);
 
-    _isGroupBy = new RadioButton(context) { Text = "Group by" };
+    _isGroupBy = new RadioButton(context) { Text = "Group by", Checked = dataContext.IsGroupBy };
     _isGroupBy.CheckedChange += _isGroupByCheckedChange;
 
-    _isThenBy = new RadioButton(context) { Text = "Group by - Then by" };
+    _isThenBy = new RadioButton(context) { Text = "Group by - Then by", Checked = dataContext.IsThenBy };
     _isThenBy.CheckedChange += _isThenByCheckedChange;
 
     var groupMode = new RadioGroup(context) { Orientation = Orientation.Horizontal };
-    groupMode.SetPadding(context.Resources!.GetDimensionPixelSize(Resource.Dimension.general_padding));
+    groupMode.SetPadding(DimensU.Spacing);
     groupMode.AddView(_isGroupBy);
     groupMode.AddView(_isThenBy);
 
-    _isRecursive = new CheckBox(context) { Text = "Group recursive" };
+    _isRecursive = new CheckBox(context) { Text = "Group recursive", Checked = dataContext.IsRecursive };
     _isRecursive.CheckedChange += _isRecursiveCheckedChange;
 
-    AddView(_groupByItems, new LayoutParams(ViewGroup.LayoutParams.MatchParent, DisplayU.DpToPx(300), 1f));
-    AddView(groupMode, new LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent));
+    AddView(_groupByItems, new LayoutParams(LPU.Match, DisplayU.DpToPx(300), 1f));
+    AddView(groupMode, new LayoutParams(LPU.Wrap, LPU.Wrap));
     AddView(_isRecursive);
-  }
-
-  public View Bind(Dialog dataContext) {
-    if (dataContext is not GroupByDialog vm) throw new InvalidOperationException();
-    DataContext = vm;
-    _isGroupBy.Checked = vm.IsGroupBy;
-    _isThenBy.Checked = vm.IsThenBy;
-    _isRecursive.Checked = vm.IsRecursive;
-
-    return this;
   }
 
   protected override void Dispose(bool disposing) {
@@ -70,17 +58,14 @@ public sealed class GroupByDialogV : LinearLayout, IDialogContentV {
   }
 
   private void _isGroupByCheckedChange(object? sender, CompoundButton.CheckedChangeEventArgs e) {
-    if (DataContext == null) return;
     DataContext.IsGroupBy = e.IsChecked;
   }
 
   private void _isThenByCheckedChange(object? sender, CompoundButton.CheckedChangeEventArgs e) {
-    if (DataContext == null) return;
     DataContext.IsThenBy = e.IsChecked;
   }
 
   private void _isRecursiveCheckedChange(object? sender, CompoundButton.CheckedChangeEventArgs e) {
-    if (DataContext == null) return;
     DataContext.IsRecursive = e.IsChecked;
   }
 }
