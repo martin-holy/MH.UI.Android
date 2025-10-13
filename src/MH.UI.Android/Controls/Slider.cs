859ee1e58c6c8c64fe77975dad22843a8f76b1dd
martin-holy <martin.holy@hotmail.com>
2025-10-13 15:32:25 +0200

    [U] Slider

--- src/MH.UI.Android/Controls/Slider.cs
+++ src/MH.UI.Android/Controls/Slider.cs
@@ -1,31 +1,82 @@
 ﻿using Android.Content;
 using Android.Widget;
+using MH.Utils;
 using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
 
 namespace MH.UI.Android.Controls;
 
-// TODO TwoWay binding for progress
 public class Slider : SeekBar {
   private readonly double _minD;
-  private readonly Action<double> _onChanged;
   private bool _disposed;
+  private Action? _disposeBinding;
 
-  public Slider(Context context, double minD, double maxD, double progressD, Action<double> onChanged) : base(context) {
+  public Slider(Context context, double minD, double maxD) : base(context) {
     _minD = minD;
-    _onChanged = onChanged;
     Max = (int)((maxD - minD) * 10);
-    Progress = (int)((progressD - minD) * 10);
-    ProgressChanged += _onProgressChanged;
   }
 
-  private void _onProgressChanged(object? sender, ProgressChangedEventArgs e) {
-    _onChanged(e.Progress / 10.0 + _minD);
+  public Slider BindProgress<TSource, TProp>(
+    TSource source,
+    Expression<Func<TSource, TProp>> propertyExpression,
+    BindingU.Mode mode = BindingU.Mode.TwoWay)
+    where TSource : class, INotifyPropertyChanged {
+
+    if (propertyExpression.Body is not MemberExpression m)
+      throw new ArgumentException("Expression must be a property access", nameof(propertyExpression));
+
+    var propertyName = m.Member.Name;
+    var getter = BindingU.GetterCache.GetGetter<TSource, TProp>(propertyName);
+    var setter = BindingU.SetterCache.GetSetter<TSource, TProp>(propertyName);
+
+    IDisposable? sub = null;
+    EventHandler<ProgressChangedEventArgs>? handler = null;
+    bool updating = false;
+
+    // --- ViewModel → Slider (OneWay)
+    sub = BindingU.Bind(this, source, propertyExpression, (v, p) => {
+      updating = true;
+      try {
+        double value = Convert.ToDouble(p);
+        var newProgress = (int)((value - v._minD) * 10);
+        if (v.Progress != newProgress)
+          v.Progress = newProgress;
+      }
+      finally { updating = false; }
+    });
+
+    // --- Slider → ViewModel (TwoWay)
+    if (mode == BindingU.Mode.TwoWay) {
+      handler = (_, e) => {
+        if (updating) return;
+        var newVal = e.Progress / 10.0 + _minD;
+
+        object converted = typeof(TProp) == typeof(int)
+          ? (object)(int)Math.Round(newVal)
+          : (object)newVal;
+
+        setter(source, (TProp)converted);
+      };
+      ProgressChanged += handler;
+    }
+
+    var oldDispose = _disposeBinding;
+    _disposeBinding = () => {
+      sub?.Dispose();
+      if (handler != null)
+        ProgressChanged -= handler;
+      oldDispose?.Invoke();
+    };
+
+    return this;
   }
 
   protected override void Dispose(bool disposing) {
     if (_disposed) return;
     if (disposing) {
-      ProgressChanged -= _onProgressChanged;
+      _disposeBinding?.Invoke();
+      _disposeBinding = null;
     }
     _disposed = true;
     base.Dispose(disposing);
