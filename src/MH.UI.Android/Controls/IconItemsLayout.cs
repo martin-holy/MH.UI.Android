c90f0864c33d4a43910eaf62b1737ed90af54b13
martin-holy <martin.holy@hotmail.com>
2025-12-04 19:30:24 +0100

    [U] IconItemsLayout: MaxHeight

--- src/MH.UI.Android/Controls/IconItemsLayout.cs
+++ src/MH.UI.Android/Controls/IconItemsLayout.cs
@@ -9,6 +9,7 @@ namespace MH.UI.Android.Controls;
 
 public class IconItemsLayout : LinearLayout {
   public WrapLayout WrapLayout { get; }
+  public int MaxHeight { get; set; } = int.MaxValue;
 
   public IconItemsLayout(Context context, string iconName, Func<object, View?> itemFactory) : base(context) {
     Orientation = Orientation.Horizontal;
@@ -24,4 +25,7 @@ public class IconItemsLayout : LinearLayout {
 
     AddView(scroll, new LayoutParams(0, LPU.Wrap, 1f));
   }
+
+  protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec) =>
+    base.OnMeasure(widthMeasureSpec, MeasureSpec.MakeMeasureSpec(MaxHeight, MeasureSpecMode.AtMost));
 }
