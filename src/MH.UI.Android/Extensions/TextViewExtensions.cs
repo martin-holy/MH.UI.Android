be6a97782cd3c4a7513a8cdc20f2a35719e73154
martin-holy <martin.holy@hotmail.com>
2026-01-04 21:18:14 +0100

    [N] TextViewExtensions: WithTextColor

---
+++ src/MH.UI.Android/Extensions/TextViewExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Android.Graphics;
+using Android.Widget;
+using AndroidX.Core.Content;
+
+namespace MH.UI.Android.Extensions;
+
+public static class TextViewExtensions {
+  public static TextView WithTextColor(this TextView textView, int colorResourceId) {
+    textView.SetTextColor(new Color(ContextCompat.GetColor(textView.Context, (int)colorResourceId)));
+    return textView;
+  }
+}
