b3d2366b47e82a04b29571d02d19072689578db9
martin-holy <martin.holy@hotmail.com>
2026-01-22 15:00:28 +0100

    [N] ImagingU: ResizeJpg

--- src/MH.UI.Android/Utils/Init.cs
+++ src/MH.UI.Android/Utils/Init.cs
@@ -4,7 +4,6 @@ using MH.UI.Android.Controls;
 
 namespace MH.UI.Android.Utils;
 
-// TODO PORT
 public static class Init {
   private static Handler? _handler;
 
@@ -14,6 +13,7 @@ public static class Init {
   }
 
   public static void SetDelegates() {
+    // TODO for users with HW keyboard
     MH.Utils.Keyboard.IsCtrlOn = () => false;
     MH.Utils.Keyboard.IsAltOn = () => false;
     MH.Utils.Keyboard.IsShiftOn = () => false;
@@ -21,11 +21,15 @@ public static class Init {
     MH.Utils.Clipboard.SetText = ClipboardU.SetText;
 
     //MH.Utils.Imaging.GetBitmapHashPixels = Imaging.GetBitmapHashPixels;
-    //MH.Utils.Imaging.ResizeJpg = Imaging.ResizeJpg;
+    MH.Utils.Imaging.ResizeJpg = _resizeJpg;
 
     MH.UI.Controls.Dialog.SetShowAsyncImplementation(DialogHost.ShowAsync);
 
     _handler = new(Looper.MainLooper!);
     MH.Utils.Tasks.Dispatch = action => _handler.Post(action);
+  }
+
+  private static void _resizeJpg(string src, string dest, int px, bool withMetadata, bool withThumbnail, int quality) {
+    ImagingU.ResizeJpg(src, dest, px, withMetadata, quality);
   }
 }
