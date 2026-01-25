e47126cac7b30be2e9c153c43f8715f59258cd4a
martin-holy <martin.holy@hotmail.com>
2026-01-25 11:42:32 +0100

    [N] ViewExtensions: WithPadding

--- src/MH.UI.Android/Extensions/ViewExtensions.cs
+++ src/MH.UI.Android/Extensions/ViewExtensions.cs
@@ -7,6 +7,16 @@ public static class ViewExtensions {
   public static void SetPadding(this View view, int value) =>
     view.SetPadding(value, value, value, value);
 
+  public static T WithPadding<T>(this T view, int value) where T : View {
+    view.SetPadding(value);
+    return view;
+  }
+
+  public static T WithPadding<T>(this T view, int left, int top, int right, int bottom) where T : View {
+    view.SetPadding(left, top, right, bottom);
+    return view;
+  }
+
   public static bool SetVisibleIf(this View view, bool value, ViewStates ifNot = ViewStates.Gone) {
     if (value) {
       view.Visibility = ViewStates.Visible;
