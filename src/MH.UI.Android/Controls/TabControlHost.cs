7fb05553efcc318eedfaef243fa0cbf6bb481925
martin-holy <martin.holy@hotmail.com>
2026-01-30 10:23:00 +0100

    [N] TabControlHost: Slot for custom View in TabStrip

--- src/MH.UI.Android/Controls/TabControlHost.cs
+++ src/MH.UI.Android/Controls/TabControlHost.cs
@@ -6,8 +6,8 @@ using MH.UI.Android.Extensions;
 using MH.UI.Android.Utils;
 using MH.UI.Controls;
 using MH.Utils;
-using MH.Utils.BaseClasses;
 using MH.Utils.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -15,6 +15,7 @@ using System.Linq;
 namespace MH.UI.Android.Controls;
 
 public class TabControlHost : LinearLayout {
+  private readonly LinearLayout _headerPanel;
   private readonly RecyclerView _tabHeaders;
   private readonly FrameLayout _tabContent;
   private readonly TabControlHostHeaderAdapter _adapter;
@@ -24,28 +25,56 @@ public class TabControlHost : LinearLayout {
   public TabControl DataContext { get; }
   public TreeMenu ItemMenu { get; }
 
-  public TabControlHost(Context context, TabControl dataContext) : base(context) {
+  public TabControlHost(Context context, TabControl dataContext, Func<object?, View?>? slotFactory = null) : base(context) {
     DataContext = dataContext;
     ItemMenu = new TreeMenu(context, dataContext.ItemMenuFactory);
-    Orientation = Orientation.Vertical;
     SetBackgroundResource(Resource.Color.c_static_ba);
 
+    _headerPanel = new LinearLayout(context);
+    _tabContent = new FrameLayout(context);
     _adapter = new TabControlHostHeaderAdapter(this);
 
     _tabHeaders = new RecyclerView(context) { ScrollBarStyle = ScrollbarStyles.OutsideOverlay };
-    _tabHeaders.SetLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.Horizontal, false));
     _tabHeaders.SetPadding(DimensU.Spacing);
     _tabHeaders.SetAdapter(_adapter);
 
-    _tabContent = new FrameLayout(context);
+    if (dataContext.TabStrip.Placement is Dock.Top or Dock.Bottom)
+      _createVertical();
+    else
+      _createHorizontal();
 
-    AddView(_tabHeaders, new LayoutParams(LPU.Match, LPU.Wrap));
-    AddView(_tabContent, new LayoutParams(LPU.Match, 0, 1));
+    if (slotFactory?.Invoke(dataContext.TabStrip.Slot) is View slot) {
+      _headerPanel.AddView(
+        slot,
+        dataContext.TabStrip.SlotPlacement is Dock.Left or Dock.Top ? 0 : -1,
+        new LinearLayout.LayoutParams(LPU.Wrap, LPU.Wrap));
+    }
 
     this.Bind(DataContext, nameof(TabControl.Tabs), x => x.Tabs, (_, _, e) => _onTabsChanged(null, e));
     this.Bind(DataContext, nameof(TabControl.Selected), x => x.Selected, (_, _) => _updateContent());
     _updateContent();
   }
+
+  private void _createHorizontal() {
+    Orientation = Orientation.Horizontal;
+    _tabHeaders.SetLayoutManager(new LinearLayoutManager(Context, LinearLayoutManager.Vertical, false));
+    _headerPanel.Orientation = Orientation.Vertical;
+    _headerPanel.AddView(_tabHeaders, new LinearLayout.LayoutParams(LPU.Wrap, 0, 1f));
+    AddView(_tabContent, new LayoutParams(0, LPU.Match, 1));
+    AddView(_headerPanel, _getViewIndex(DataContext.TabStrip.Placement), new LayoutParams(LPU.Wrap, LPU.Match));
+  }
+
+  private void _createVertical() {
+    Orientation = Orientation.Vertical;
+    _tabHeaders.SetLayoutManager(new LinearLayoutManager(Context, LinearLayoutManager.Horizontal, false));
+    _headerPanel.Orientation = Orientation.Horizontal;
+    _headerPanel.AddView(_tabHeaders, new LinearLayout.LayoutParams(0, LPU.Wrap, 1f));
+    AddView(_tabContent, new LayoutParams(LPU.Match, 0, 1));
+    AddView(_headerPanel, _getViewIndex(DataContext.TabStrip.Placement), new LayoutParams(LPU.Match, LPU.Wrap));
+  }
+
+  private int _getViewIndex(Dock dock) =>
+    dock is Dock.Left or Dock.Top ? 0 : -1;
 
   protected virtual View? _getItemView(LinearLayout container, object? item) => null;
 
