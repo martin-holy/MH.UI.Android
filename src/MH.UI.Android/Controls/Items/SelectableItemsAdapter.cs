fc08a526610966df00deab9d8cd35918f7b84a6b
martin-holy <martin.holy@hotmail.com>
2026-02-14 09:27:59 +0100

    [N] SelectableItemsView

---
+++ src/MH.UI.Android/Controls/Items/SelectableItemsAdapter.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using Android.Views;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+
+namespace MH.UI.Android.Controls.Items;
+
+public class SelectableItemsAdapter<T> : ItemsAdapter<T> where T : class {
+  public SelectableItemsAdapter(SelectableItemsView<T> owner, Func<Context, View> createItemView) : base(owner, createItemView) { }
+
+  public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position, IList<Java.Lang.Object> payloads) {
+    if (payloads.Count == 0) {
+      base.OnBindViewHolder(holder, position, payloads);
+      return;
+    }
+
+    var selection = ((SelectableItemsView<T>)_owner).Selection;
+
+    foreach (var payload in payloads) {
+      if (Equals(payload, SelectableItemsView<T>._selectionPayload))
+        holder.ItemView.Selected = selection.IsSelected(_owner._items![position]);
+    }
+  }
+}
---
+++ src/MH.UI.Android/Controls/Items/SelectableItemsView.cs
@@ -0,0 +1,39 @@
+﻿using Android.Content;
+using Android.Views;
+using AndroidX.RecyclerView.Widget;
+using MH.Utils;
+using MH.Utils.Collections;
+using System;
+
+namespace MH.UI.Android.Controls.Items;
+
+public class SelectableItemsView<T> : ItemsViewBase<T> where T : class {
+  internal static readonly Java.Lang.Object _selectionPayload = new Java.Lang.String("selection");
+
+  public SelectionManager<T> Selection { get; }
+
+  public SelectableItemsView(Context context, Func<Context, View> createItemView) : base(context) {
+    ItemsAdapter = new SelectableItemsAdapter<T>(this, createItemView);
+    SetLayoutManager(new LinearLayoutManager(context));
+    SetAdapter(ItemsAdapter);
+
+    Selection = new SelectionManager<T>(ReferenceEqualityComparer<T>.Instance);
+    Selection.ItemSelectionChanged += (item, _) => _notifyItemSelectionChange(item);
+    Selection.Cleared += items => {
+      foreach (var item in items) {
+        _notifyItemSelectionChange(item);
+      }
+    };
+  }
+
+  private void _notifyItemSelectionChange(T item) {
+    var index = _items!.IndexOf(item);
+    if (index >= 0)
+      ItemsAdapter?.NotifyItemChanged(index, _selectionPayload);
+  }
+
+  internal override void HandleItemClick(T item) {
+    base.HandleItemClick(item);
+    Selection.Toggle(item);
+  }
+}
