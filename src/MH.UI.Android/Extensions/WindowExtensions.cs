8bdf12c977195dec3baefdc4fa79fb1654e1cb1a
martin-holy <martin.holy@hotmail.com>
2026-02-16 09:04:55 +0100

    [N] WindowExtensions: Enter and exit full screen methods

---
+++ src/MH.UI.Android/Extensions/WindowExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Android.OS;
+using Android.Views;
+
+namespace MH.UI.Android.Extensions;
+
+public static class WindowExtensions {
+  public static void EnterFullScreen(this Window window) {
+    if (Build.VERSION.SdkInt >= BuildVersionCodes.R) {
+      window.SetDecorFitsSystemWindows(false);
+      if (window.InsetsController is { } controller) {
+        controller.Hide(WindowInsets.Type.SystemBars());
+        controller.SystemBarsBehavior = (int)WindowInsetsControllerBehavior.ShowTransientBarsBySwipe;
+      }
+    }
+    else {
+      window.AddFlags(WindowManagerFlags.Fullscreen);
+      window.DecorView.SystemUiVisibility = (StatusBarVisibility)(
+        SystemUiFlags.ImmersiveSticky |
+        SystemUiFlags.HideNavigation |
+        SystemUiFlags.Fullscreen |
+        SystemUiFlags.LayoutFullscreen |
+        SystemUiFlags.LayoutHideNavigation |
+        SystemUiFlags.LayoutStable);
+    }
+  }
+
+  public static void ExitFullScreen(this Window window) {
+    if (Build.VERSION.SdkInt >= BuildVersionCodes.R) {
+      window.InsetsController?.Show(WindowInsets.Type.SystemBars());
+      window.SetDecorFitsSystemWindows(true);
+    }
+    else {
+      window.ClearFlags(WindowManagerFlags.Fullscreen);
+      window.DecorView.SystemUiVisibility = StatusBarVisibility.Visible;
+    }
+  }
+}
