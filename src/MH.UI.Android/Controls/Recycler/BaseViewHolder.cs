1b3ccbf64314c0b07cfa0c767466d60023b081b9
martin-holy <martin.holy@hotmail.com>
2026-02-18 14:55:27 +0100

    [N] BaseViewHolder

---
+++ src/MH.UI.Android/Controls/Recycler/BaseViewHolder.cs
@@ -0,0 +1,11 @@
+﻿using Android.Views;
+using AndroidX.RecyclerView.Widget;
+using MH.UI.Android.Utils;
+
+namespace MH.UI.Android.Controls.Recycler;
+
+public class BaseViewHolder : RecyclerView.ViewHolder {
+  public BaseViewHolder(View view, RecyclerView.LayoutParams viewLayoutParams) : base(view) {
+    view.LayoutParameters = viewLayoutParams;
+  }
+}
