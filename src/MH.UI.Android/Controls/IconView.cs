d0e4ba4c7507ff5ab93a75e1eb7dabb8e7058f23
martin-holy <martin.holy@hotmail.com>
2026-02-26 13:39:54 +0100

    [B] IconView: Enabled color change

--- src/MH.UI.Android/Controls/IconView.cs
+++ src/MH.UI.Android/Controls/IconView.cs
@@ -20,16 +20,6 @@ public class IconView : ImageView {
     _setDrawable(drawableId == null ? null : ContextCompat.GetDrawable(Context, (int)drawableId), colorId);
 
   private IconView _setDrawable(Drawable? drawable, int? colorId) {
-    if (drawable != null) {
-      drawable.Mutate();
-
-      int tintColor = Enabled
-        ? (colorId != null ? ContextCompat.GetColor(Context, (int)colorId) : 0)
-        : ContextCompat.GetColor(Context, Resource.Color.c_disabled_fo);
-
-      if (tintColor != 0) drawable.SetTint(tintColor);
-    }
-
     SetImageDrawable(drawable);
     return this;
   }
