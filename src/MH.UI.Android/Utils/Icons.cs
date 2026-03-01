4d3683fca57a52d53ff6f4edfa85b625c6849211
martin-holy <martin.holy@hotmail.com>
2026-03-01 17:32:49 +0100

    [U] Icons: GetIcon methods

--- src/MH.UI.Android/Utils/Icons.cs
+++ src/MH.UI.Android/Utils/Icons.cs
@@ -20,18 +20,43 @@ public static class Icons {
   public static Func<string, string> ConvertIconName { get; set; } = (iconName) => iconName.ToSnakeCase();
 
   public static Drawable? GetDrawable(Context? context, string? iconName) {
-    if (context == null || context.Resources == null || iconName == null) return null;
+    if (context?.Resources == null || iconName == null) return null;
     var id = context.Resources.GetIdentifier(ConvertIconName(iconName), "drawable", context.PackageName);
     if (id == 0) return null;
     return ContextCompat.GetDrawable(context, id);
   }
 
-  public static Drawable? GetIcon(Context? context, string? iconName, Dictionary<object, object>? iconNameToColor = null) {
-    if (context == null || iconName == null) return null;
-    if (GetDrawable(context, iconName) is not { } drawable) return null;
+  public static Drawable? GetIcon(Context? context, string? iconName, int? colorId = null) =>
+    _getIcon(context, iconName, colorId, null);
 
+  public static Drawable? GetIcon(Context? context, string? iconName, Dictionary<object, object> iconNameToColor) =>
+    _getIcon(context, iconName, null, iconNameToColor);
+
+  public static Drawable? GetIcon(Context? context, int drawableId, int? colorId = null) =>
+    _getIcon(context, drawableId, colorId, null);
+
+  public static Drawable? GetIcon(Context? context, int drawableId, Dictionary<object, object> iconNameToColor) =>
+    _getIcon(context, drawableId, null, iconNameToColor);
+
+  private static Drawable? _getIcon(Context? context, string? iconName, int? colorId, Dictionary<object, object>? iconNameToColor) {
+    if (GetDrawable(context, iconName) is not { } drawable) return null;
+    var color = colorId != null
+      ? ContextCompat.GetColor(context, (int)colorId)
+      : GetColor(context, iconName!, iconNameToColor ?? IconNameToColor);
+    return _applyColor(context!, drawable, color);
+  }
+
+  private static Drawable? _getIcon(Context? context, int drawableId, int? colorId, Dictionary<object, object>? iconNameToColor) {
+    if (ContextCompat.GetDrawable(context, drawableId) is not { } drawable) return null;
+    var color = colorId != null
+      ? ContextCompat.GetColor(context, (int)colorId)
+      : GetColor(context, "default", iconNameToColor ?? IconNameToColor);
+    return _applyColor(context!, drawable, color);
+  }
+
+  private static Drawable? _applyColor(Context context, Drawable drawable, int color) {
     var colors = new int[] {
-      GetColor(context, iconName, iconNameToColor ?? IconNameToColor),
+      color,
       ContextCompat.GetColor(context, Resource.Color.c_disabled_fo)
     };
 
@@ -40,7 +65,7 @@ public static class Icons {
     return drawable;
   }
 
-  public static int GetColor(Context context, string iconName, Dictionary<object, object>? iconNameToColor) {
+  public static int GetColor(Context? context, string iconName, Dictionary<object, object>? iconNameToColor) {
     if (iconNameToColor == null
       || (!iconNameToColor.TryGetValue(iconName, out var colorId)
       && !iconNameToColor.TryGetValue("default", out colorId)))
