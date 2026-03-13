a6f2066891890be17b75edc971eb07b503e202c9
martin-holy <martin.holy@hotmail.com>
2026-03-13 10:25:54 +0100

    [N] LPU: LinearLayout.LayoutParams factory methods

--- src/MH.UI.Android/Utils/LPU.cs
+++ src/MH.UI.Android/Utils/LPU.cs
@@ -1,8 +1,21 @@
 ﻿using Android.Views;
+using Android.Widget;
 
 namespace MH.UI.Android.Utils;
 
 public static class LPU {
   public const int Match = ViewGroup.LayoutParams.MatchParent;
   public const int Wrap = ViewGroup.LayoutParams.WrapContent;
+
+  public static LinearLayout.LayoutParams Linear(int w, int h) =>
+    new(w, h);
+
+  public static LinearLayout.LayoutParams Linear(int w, int h, float weight) =>
+    new(w, h, weight);
+
+  public static LinearLayout.LayoutParams Linear(int w, int h, GravityFlags gravity) =>
+    new(w, h) { Gravity = gravity };
+
+  public static LinearLayout.LayoutParams Linear(int w, int h, float weight, GravityFlags gravity) =>
+    new(w, h, weight) { Gravity = gravity };
 }
