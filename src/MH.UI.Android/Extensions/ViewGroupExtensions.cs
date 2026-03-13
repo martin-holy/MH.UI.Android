0108b954f3aed81725b992105fedee13626896d1
martin-holy <martin.holy@hotmail.com>
2026-03-13 10:23:07 +0100

    [N] ViewGroupExtensions: Add

---
+++ src/MH.UI.Android/Extensions/ViewGroupExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Android.Views;
+
+namespace MH.UI.Android.Extensions;
+
+public static class ViewGroupExtensions {
+  public static TParent Add<TParent>(this TParent parent, View child, ViewGroup.LayoutParams lp)
+    where TParent : ViewGroup {
+    parent.AddView(child, lp);
+    return parent;
+  }
+}
