19e771a7cb3e1bc708fd87d213741049624c7ba4
martin-holy <martin.holy@hotmail.com>
2026-03-13 10:24:13 +0100

    [N] LayoutU: Horizontal and Vertical LinearLayout

---
+++ src/MH.UI.Android/Utils/LayoutU.cs
@@ -0,0 +1,12 @@
+﻿using Android.Content;
+using Android.Widget;
+
+namespace MH.UI.Android.Utils;
+
+public static class LayoutU {
+  public static LinearLayout Horizontal(Context? context) =>
+    new(context) { Orientation = Orientation.Horizontal };
+
+  public static LinearLayout Vertical(Context? context) =>
+    new(context) { Orientation = Orientation.Vertical };
+}
