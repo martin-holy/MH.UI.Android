604f876f860d0d491860f49c938c2f4318eabb19
martin-holy <martin.holy@hotmail.com>
2026-04-06 16:01:06 +0200

    [N] ImageViewExtensions: UpdateImageBitmap

---
+++ src/MH.UI.Android/Extensions/ImageViewExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Widget;
+
+namespace MH.UI.Android.Extensions;
+
+public static class ImageViewExtensions {
+  public static void UpdateImageBitmap(this ImageView view, Bitmap? bitmap) {
+    var oldBitmap = view.Drawable is BitmapDrawable bd ? bd.Bitmap : null;
+    view.SetImageBitmap(bitmap);
+    if (oldBitmap?.IsRecycled == false && bitmap != oldBitmap) oldBitmap.Recycle();
+  }
+}
