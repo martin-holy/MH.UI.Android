cf6afa2e9e9c7b205c8519a1f00322747a60f078
martin-holy <martin.holy@hotmail.com>
2026-04-11 14:50:09 +0200

    [N] BindableAdapter: Payloads handling

--- src/MH.UI.Android/Controls/Recycler/BindableAdapter.cs
+++ src/MH.UI.Android/Controls/Recycler/BindableAdapter.cs
@@ -12,17 +12,20 @@ public class BindableAdapter<T> : RecyclerView.Adapter {
   private readonly Func<Context, View> _createView;
   private readonly Func<RecyclerView.LayoutParams> _createLayoutParams;
   private readonly Action<View>? _onViewCreated;
+  private readonly Func<View, int, IList<Java.Lang.Object>, bool>? _onPayloadBind;
 
   public BindableAdapter(
     Func<IReadOnlyList<T>> getItems,
     Func<Context, View> createView,
     Func<RecyclerView.LayoutParams> createLayoutParams,
-    Action<View>? onViewCreated = null) {
+    Action<View>? onViewCreated = null,
+    Func<View, int, IList<Java.Lang.Object>, bool>? onPayloadBind = null) {
 
     _getItems = getItems;
     _createView = createView;
     _createLayoutParams = createLayoutParams;
     _onViewCreated = onViewCreated;
+    _onPayloadBind = onPayloadBind;
   }
 
   public override int ItemCount => _getItems().Count;
@@ -35,6 +38,18 @@ public class BindableAdapter<T> : RecyclerView.Adapter {
 
   public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position) =>
     (holder.ItemView as IBindable<T>)?.Rebind(_getItems()[position]);
+
+  public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position, IList<Java.Lang.Object> payloads) {
+    if (payloads.Count == 0) {
+      base.OnBindViewHolder(holder, position, payloads);
+      return;
+    }
+
+    if (_onPayloadBind?.Invoke(holder.ItemView, position, payloads) == true)
+      return;
+
+    base.OnBindViewHolder(holder, position, payloads);
+  }
 
   public override void OnViewRecycled(Java.Lang.Object holder) {
     (((RecyclerView.ViewHolder)holder).ItemView as IBindable<T>)?.Unbind();
