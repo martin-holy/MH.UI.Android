cc8d4fa3cc9fa1be62d04dbb2d252e79dab7ff63
martin-holy <martin.holy@hotmail.com>
2026-04-15 16:13:05 +0200

    [U] Slider: using ValueSelector

--- src/MH.UI.Android/Controls/Slider.cs
+++ src/MH.UI.Android/Controls/Slider.cs
@@ -1,17 +1,77 @@
 ﻿using Android.Content;
 using Android.Widget;
+using MH.UI.Controls;
+using System;
+using System.ComponentModel;
 
 namespace MH.UI.Android.Controls;
 
 public class Slider : SeekBar {
-  public double MinD { get; }
-  public double Scale { get; }
-  public double TickFrequency { get; }  
+  private bool _updatingFromModel;
 
-  public Slider(Context? context, double minD, double maxD, double tickFrequency) : base(context) {
-    MinD = minD;
-    TickFrequency = tickFrequency <= 0 ? (maxD - minD) / 100 : tickFrequency;
-    Scale = 1.0 / TickFrequency;
-    Max = (int)((maxD - minD) * Scale);
+  public ValueSelector Selector { get; }
+
+  public Slider(Context? context) : this(context, new ValueSelector()) { }
+
+  public Slider(Context? context, double minimum, double maximum, double tickFrequency)
+    : this(context, new() { Minimum = minimum, Maximum = maximum, TickFrequency = tickFrequency }) { }
+
+  public Slider(Context? context, ValueSelector selector) : base(context) {
+    Selector = selector;
+
+    Selector.PropertyChanged += _onSelectorPropertyChanged;
+    ProgressChanged += _onProgressChanged;
+
+    _refreshRange();
+    _refreshValue();
+  }
+
+  private void _onSelectorPropertyChanged(object? sender, PropertyChangedEventArgs e) {
+    switch (e.PropertyName) {
+      case nameof(ValueSelector.Minimum):
+      case nameof(ValueSelector.Maximum):
+      case nameof(ValueSelector.TickFrequency):
+        _refreshRange();
+        _refreshValue();
+        break;
+
+      case nameof(ValueSelector.Value):
+        _refreshValue();
+        break;
+    }
+  }
+
+  private void _onProgressChanged(object? sender, ProgressChangedEventArgs e) {
+    if (_updatingFromModel || !e.FromUser) return;
+    Selector.Value = _fromProgress(e.Progress);
+  }
+
+  private void _refreshRange() {
+    Max = _toProgress(Selector.Maximum);
+  }
+
+  private void _refreshValue() {
+    var progress = _toProgress(Selector.Value);
+
+    if (Progress == progress) return;
+
+    _updatingFromModel = true;
+    Progress = progress;
+    _updatingFromModel = false;
+  }
+
+  private int _toProgress(double value) =>
+    (int)Math.Round((value - Selector.Minimum) / Selector.TickFrequency);
+
+  private double _fromProgress(int progress) =>
+    Selector.Minimum + progress * Selector.TickFrequency;
+
+  protected override void Dispose(bool disposing) {
+    if (disposing) {
+      Selector.PropertyChanged -= _onSelectorPropertyChanged;
+      ProgressChanged -= _onProgressChanged;
+    }
+
+    base.Dispose(disposing);
   }
 }
