ac8f33c5ff224fceae7f53a64734550f19b22095
martin-holy <martin.holy@hotmail.com>
2026-04-15 16:16:39 +0200

    [C] IconButton: nullable Context in ctor

--- src/MH.UI.Android/Controls/IconButton.cs
+++ src/MH.UI.Android/Controls/IconButton.cs
@@ -6,8 +6,8 @@ using Android.Widget;
 namespace MH.UI.Android.Controls;
 
 public class IconButton : ImageButton {
-  public IconButton(Context context) : this(context, null, Resource.Attribute.iconButtonStyle) { }
-  public IconButton(Context context, IAttributeSet? attrs) : this(context, attrs, Resource.Attribute.iconButtonStyle) { }
-  public IconButton(Context context, IAttributeSet? attrs, int defStyleAttr) : base(context, attrs, defStyleAttr) { }
+  public IconButton(Context? context) : this(context, null, Resource.Attribute.iconButtonStyle) { }
+  public IconButton(Context? context, IAttributeSet? attrs) : this(context, attrs, Resource.Attribute.iconButtonStyle) { }
+  public IconButton(Context? context, IAttributeSet? attrs, int defStyleAttr) : base(context, attrs, defStyleAttr) { }
   protected IconButton(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { }
 }
