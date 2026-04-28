35c209b25ed668079f6959a07963a48d7e7e765d
martin-holy <martin.holy@hotmail.com>
2026-04-28 17:44:36 +0200

    [C] SlidePanelsGrid: refactoring

---
+++ src/MH.UI.Android/Controls/Hosts/SlidePanelsGridHost/SlidePanelHost.cs
@@ -0,0 +1,70 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+using MH.UI.Android.Extensions;
+using MH.UI.Android.Utils;
+using MH.UI.Controls;
+using MH.Utils;
+using MH.Utils.Disposables;
+using System;
+using Dock = MH.UI.Controls.Dock;
+
+namespace MH.UI.Android.Controls.Hosts.SlidePanelsGridHost;
+
+public class SlidePanelHost : FrameLayout {
+  private readonly SlidePanel _dataContext;
+
+  public SlidePanelHost(Context context, View content, SlidePanel dataContext, BindingScope bindings) : base(context) {
+    _dataContext = dataContext;
+    _dataContext.Bind(nameof(SlidePanel.IsOpen), x => x.IsOpen, _ => _refreshPosition(true), false).DisposeWith(bindings);
+    this.WithClickAction(_onClick);
+    AddView(content, LPU.FrameMatch());
+  }
+
+  private static void _onClick(SlidePanelHost self) =>
+    self._dataContext.TogglePinned();
+
+  protected override void OnSizeChanged(int w, int h, int oldw, int oldh) {
+    base.OnSizeChanged(w, h, oldw, oldh);
+    _dataContext.Size = _dataContext.Dock is Dock.Top or Dock.Bottom ? h : w;
+    _refreshPosition(false);
+  }
+
+  private void _refreshPosition(bool animated) {
+    float targetX = 0f;
+    float targetY = 0f;
+
+    if (!_dataContext.IsOpen) {
+      switch (_dataContext.Dock) {
+        case Dock.Left: targetX = -Width; break;
+        case Dock.Right: targetX = Width; break;
+        case Dock.Top: targetY = -Height; break;
+        case Dock.Bottom: targetY = Height; break;
+      }
+    }
+
+    if (animated)
+      _animateTo(targetX, targetY);
+    else
+      _setTransform(targetX, targetY);
+  }
+
+  private void _animateTo(float x, float y) {
+    Animate()?.Cancel();
+
+    var distance = Math.Max(Math.Abs(TranslationX - x), Math.Abs(TranslationY - y));
+    var ms = Math.Max(120, (long)(distance * 0.7));
+
+    Animate()?
+      .TranslationX(x)
+      .TranslationY(y)
+      .SetDuration(ms)
+      .Start();
+  }
+
+  private void _setTransform(float x, float y) {
+    Animate()?.Cancel();
+    TranslationX = x;
+    TranslationY = y;
+  }
+}
--- src/MH.UI.Android/Controls/Hosts/SlidePanelsGridHost/SlidePanelsGridHost.cs
+++ src/MH.UI.Android/Controls/Hosts/SlidePanelsGridHost/SlidePanelsGridHost.cs
@@ -1,43 +1,84 @@
 ﻿using Android.Content;
 using Android.Views;
 using Android.Widget;
-using MH.UI.Android.Binding;
 using MH.UI.Android.Utils;
 using MH.UI.Controls;
+using MH.Utils;
 using MH.Utils.Disposables;
 
 namespace MH.UI.Android.Controls.Hosts.SlidePanelsGridHost;
 
-public class SlidePanelsGridHost : LinearLayout {
-  private readonly LoopPager _viewPager;
-  private readonly View _panelLeft;
-  private readonly View _panelTop;
-  private readonly View _panelRight;
-  private readonly View _panelBottom;
-  private readonly View _panelMiddle;
+public enum TopAndBottomPanelsPlacement { Global, MiddleOnly }
 
-  public LoopPager ViewPager { get => _viewPager; }
+public class SlidePanelsGridHost : FrameLayout {
+  private readonly View _panelLeft;
+  private readonly SlidePanelHost _panelTop;
+  private readonly View _panelRight;
+  private readonly SlidePanelHost _panelBottom;
+  private readonly View _panelMiddle;
+  private readonly FrameLayout _pagerHost;
+  private readonly View _middleContent;
+  private readonly TopAndBottomPanelsPlacement _gridMode;
+
+  public LoopPager ViewPager { get; }
   public SlidePanelsGrid DataContext { get; }
 
-  public SlidePanelsGridHost(Context context, SlidePanelsGrid dataContext, BindingScope bindings,
+  public SlidePanelsGridHost(Context context, SlidePanelsGrid dataContext, BindingScope bindings, TopAndBottomPanelsPlacement gridMode,
     View left, View top, View right, View bottom, View middle) : base(context) {
-
-    DataContext = dataContext;
-    _panelLeft = left;
-    _panelTop = top;
-    _panelRight = right;
-    _panelBottom = bottom;
-    _panelMiddle = middle;
-    _viewPager = new LoopPager(context, [_panelLeft, _panelMiddle, _panelRight]);
-
-    Orientation = Orientation.Vertical;
     SetBackgroundResource(Resource.Color.c_static_ba);
 
-    _panelTop.BindVisibility(dataContext.PanelTop, nameof(SlidePanel.IsPinned), x => x.IsPinned, bindings);
-    _panelBottom.BindVisibility(dataContext.PanelBottom, nameof(SlidePanel.IsPinned), x => x.IsPinned, bindings);
+    DataContext = dataContext;
+    _gridMode = gridMode;
+    _middleContent = middle;
 
-    AddView(top, LPU.LinearMatchWrap());
-    AddView(_viewPager, LPU.Linear(LPU.Match, 0, 1f));
-    AddView(bottom, LPU.LinearMatchWrap());
+    _panelLeft = left;
+    _panelTop = new(context, top, dataContext.PanelTop, bindings);
+    _panelRight = right;
+    _panelBottom = new(context, bottom, dataContext.PanelBottom, bindings);
+
+    if (gridMode == TopAndBottomPanelsPlacement.MiddleOnly) {
+      var middleWrapper = new FrameLayout(context);
+      middleWrapper.AddView(middle, LPU.FrameMatch());
+      middleWrapper.AddView(_panelTop, LPU.Frame(LPU.Match, LPU.Wrap, GravityFlags.Top));
+      middleWrapper.AddView(_panelBottom, LPU.Frame(LPU.Match, LPU.Wrap, GravityFlags.Bottom));
+      _panelMiddle = middleWrapper;
+    }
+    else
+      _panelMiddle = middle;
+
+    ViewPager = new LoopPager(context, [_panelLeft, _panelMiddle, _panelRight]);
+
+    _pagerHost = new FrameLayout(context);
+    _pagerHost.AddView(ViewPager, LPU.FrameMatch());
+    AddView(_pagerHost, LPU.FrameMatch());
+
+    if (gridMode == TopAndBottomPanelsPlacement.Global) {
+      AddView(_panelTop, LPU.Frame(LPU.Match, LPU.Wrap, GravityFlags.Top));
+      AddView(_panelBottom, LPU.Frame(LPU.Match, LPU.Wrap, GravityFlags.Bottom));
+    }
+
+    _initPanel(DataContext.PanelTop, bindings);
+    _initPanel(DataContext.PanelBottom, bindings);
+  }
+
+  private void _initPanel(SlidePanel panel, BindingScope bindings) {
+    panel.Bind(nameof(SlidePanel.IsPinned), x => x.IsPinned, _ => _updateLayout(), false).DisposeWith(bindings);
+    panel.Bind(nameof(SlidePanel.Size), x => x.Size, _ => _updateLayout(), false).DisposeWith(bindings);
+  }
+
+  private void _updateLayout() =>
+    _updateMiddleMargins(_gridMode == TopAndBottomPanelsPlacement.MiddleOnly ? _middleContent : _pagerHost);
+
+  private void _updateMiddleMargins(View view) {
+    int top = DataContext.PanelTop.IsPinned ? _panelTop.Height : 0;
+    int bottom = DataContext.PanelBottom.IsPinned ? _panelBottom.Height : 0;
+
+    if (view.LayoutParameters is not MarginLayoutParams lp) return;
+    if (lp.TopMargin == top && lp.BottomMargin == bottom) return;
+
+    lp.TopMargin = top;
+    lp.BottomMargin = bottom;
+
+    view.LayoutParameters = lp;
   }
 }
