9416dbf2e7f084e7eef47377bcb58b26078f0890
martin-holy <martin.holy@hotmail.com>
2026-05-02 11:04:38 +0200

    [N] IconItemsViewBase

---
+++ src/MH.UI.Android/Controls/Items/IconItemsViewBase.cs
@@ -0,0 +1,18 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+using MH.UI.Android.Extensions;
+using MH.UI.Android.Utils;
+
+namespace MH.UI.Android.Controls.Items;
+
+public abstract class IconItemsViewBase : LinearLayout {
+  public IconItemsViewBase(Context context, string iconName) : base(context) {
+    Orientation = Orientation.Horizontal;
+    SetGravity(GravityFlags.CenterVertical);
+
+    AddView(new IconView(context, iconName),
+      LPU.Linear(DimensU.IconSize, DimensU.IconSize)
+        .WithMargin(DimensU.Spacing, 0, DimensU.Spacing, 0));
+  }
+}
