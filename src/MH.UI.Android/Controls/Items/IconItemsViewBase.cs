0d6f46490e8933cfba449c24e732d35da99274b8
martin-holy <martin.holy@hotmail.com>
2026-05-03 14:27:36 +0200

    [U] IconItemsViewBase

--- src/MH.UI.Android/Controls/Items/IconItemsViewBase.cs
+++ src/MH.UI.Android/Controls/Items/IconItemsViewBase.cs
@@ -3,16 +3,27 @@ using Android.Views;
 using Android.Widget;
 using MH.UI.Android.Extensions;
 using MH.UI.Android.Utils;
+using System;
 
 namespace MH.UI.Android.Controls.Items;
 
 public abstract class IconItemsViewBase : LinearLayout {
-  public IconItemsViewBase(Context context, string iconName) : base(context) {
+  private object[]? _items;
+
+  protected readonly Func<object, View?> _itemFactory;
+
+  public object[]? Items { get => _items; set { _items = value; _populateItems(_items); } }
+
+  public IconItemsViewBase(Context context, string iconName, Func<object, View?> itemFactory) : base(context) {
     Orientation = Orientation.Horizontal;
     SetGravity(GravityFlags.CenterVertical);
+
+    _itemFactory = itemFactory;
 
     AddView(new IconView(context, iconName),
       LPU.Linear(DimensU.IconSize, DimensU.IconSize)
         .WithMargin(DimensU.Spacing, 0, DimensU.Spacing, 0));
   }
+
+  protected abstract void _populateItems(object[]? items);
 }
